bb2ed6b8c0abd49fe3a63ede863524f98d705a3d
Caperr <1jasperlaw@gmail.com>
2020-04-16 22:55:51 +0100

    implement A* pathfinding! It works, too!

--- bbutil.py
+++ bbutil.py
@@ -15,6 +15,17 @@ class System:
         self.security = security
         self.coordinates = coordinates
 
+    def getNeighbours(self):
+        return self.neighbours
+
+    def distanceTo(self, other):
+        return math.sqrt((other.coordinates[1] - self.coordinates[1]) ** 2 +
+                    (other.coordinates[0] - self.coordinates[0]) ** 2)
+
+    def hasJumpGate(self):
+        return bool(self.neighbours)
+
+
 def readJDB(dbFile):
     f = open(dbFile, "r")
     txt = f.read()
@@ -31,15 +42,65 @@ def writeJDB(dbFile, db):
 
 class AStarNode(System):
     syst = None
+    parent = None
     g = 0
     h = 0
     f = 0
     
-    def __init__(self, syst, g=0, h=0, f=0):
+    def __init__(self, syst, parent, g=0, h=0, f=0):
         self.syst = syst
-
+        self.parent = parent
+        self.g = g
+        self.h = h
+        self.f = g + h
 
 
 def heuristic(start, end):
-    return math.sqrt((end.coordinates[0] - start.coordinates[0]) ** 2,
-                    (end.coordinates[1] - start.coordinates[1]) ** 2)
+    return math.sqrt((end.coordinates[1] - start.coordinates[1]) ** 2 +
+                    (end.coordinates[0] - start.coordinates[0]) ** 2)
+
+
+def bbAStar(start, end, graph):
+    open = [AStarNode(graph[start], None, h=heuristic(graph[start], graph[end]))]
+    closed = []
+    count = 0
+
+    while open:
+        q = open.pop(0)
+
+        count = count + 1
+        if count == 50:
+            return "#"
+        for succName in q.syst.getNeighbours():
+            if succName == end:
+                closed.append(AStarNode(graph[succName], q))
+                route = []
+                node = closed[-1]
+                while node:
+                    route.append(node.syst.name)
+                    node = node.parent
+                return route[::-1]
+
+            succ = AStarNode(graph[succName], q)
+            succ.g = q.g + q.syst.distanceTo(succ.syst)
+            succ.h = heuristic(succ.syst, graph[end])
+            succ.f = succ.g + succ.h
+
+            betterFound = False
+            for existingNode in open + closed:
+                if existingNode.syst.coordinates == succ.syst.coordinates and existingNode.f <= succ.f:
+                    betterFound = True
+            if betterFound:
+                continue
+
+            insertPos = len(open)
+            for i in range(len(open)):
+                if open[i].f > succ.f:
+                    if i != 0:
+                        insertPos = i -1
+                    break
+            open.insert(insertPos, succ)
+
+        closed.append(succ)
+
+    return "!"
