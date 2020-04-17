a63de0ec5f53b9b118235d7cef292ff064dd9054
Caperr <1jasperlaw@gmail.com>
2020-04-17 22:31:36 +0100

    use dictionary of guild IDs to channel IDs for sendChannel

--- bbconfig.py
+++ bbconfig.py
@@ -1,5 +1,6 @@
 maxBountiesPerFaction = 5
-sendChannel = 699744305715085334
+# sendChannel = 699744305715085334
+sendChannel = {}
 botLoggedIn = False
 # bountyRoutes = [['a','b']]
 newBountyDelay = 300
