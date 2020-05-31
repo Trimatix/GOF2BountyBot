6bbf2992023a00e1c04fcfd15409092e405ca9ea
Trimatix <1jasperlaw@gmail.com>
2020-05-31 21:45:16 +0100

    allow empty faction names to indicate autogeneration

--- bbCriminal.py
+++ bbCriminal.py
@@ -8,13 +8,14 @@ class Criminal (bbutil.Aliasable):
     wiki = ""
     hasWiki = False
     isPlayer = False
+    builtIn = False
 
-    def __init__(self, name, faction, icon, isPlayer=False, aliases=[], wiki=""):
+    def __init__(self, name, faction, icon, builtIn=False, isPlayer=False, aliases=[], wiki=""):
         super(Criminal, self).__init__(name, aliases)
         if name == "":
             raise RuntimeError("CRIM_CONS_NONAM: Attempted to create a Criminal with an empty name")
-        if faction == "":
-            raise RuntimeError("CRIM_CONS_NOFAC: Attempted to create a Criminal with an empty faction")
+        # if faction == "":
+        #     raise RuntimeError("CRIM_CONS_NOFAC: Attempted to create a Criminal with an empty faction")
         if faction == "":
             raise RuntimeError("CRIM_CONS_NOICO: Attempted to create a Criminal with an empty icon")
         
@@ -24,9 +25,18 @@ class Criminal (bbutil.Aliasable):
         self.wiki = wiki
         self.hasWiki = wiki != ""
         self.isPlayer = isPlayer
+        self.builtIn = builtIn
 
     def getType(self):
         return Criminal
+
+    def toDict(self):
+        if self.builtIn:
+            print("BUILT IN")
+            data["criminal"] = {"builtIn":True, "name":self.criminal.name}
+        else:
+            print("NOT BUILT IN")
+            data["criminal"] = {"builtIn":False, "isPlayer": self.criminal.isPlayer, "name":self.criminal.name, "icon":self.criminal.icon}
 
 
 def fromDict(crimDict):
