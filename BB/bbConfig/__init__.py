72a2ef1f3529bbd2dce6a5bb013c146b92b9f9dc
Caperr <1jasperlaw@gmail.com>
2020-06-03 21:44:47 +0100

    initialise bbData.builtInCriminals/Systems using dicts (values) rather than names (keys)

--- BB/bbConfig/__init__.py
+++ BB/bbConfig/__init__.py
@@ -3,8 +3,8 @@
 from . import bbData
 from ..bbObjects import bbCriminal, bbSystem
 
-for criminalDict in bbData.builtInCriminalData:
+for criminalDict in bbData.builtInCriminalData.values():
     bbData.builtInCriminalObjs[criminalDict["name"]] = bbCriminal.fromDict(criminalDict)
 
-for systemDict in bbData.builtInSystemData:
+for systemDict in bbData.builtInSystemData.values():
     bbData.builtInSystemObjs[systemDict["name"]] = bbSystem.fromDict(systemDict)
