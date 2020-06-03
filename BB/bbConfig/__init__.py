f593c66c5900a4b65a44367bec63014352ad2e5c
Caperr <1jasperlaw@gmail.com>
2020-06-03 23:36:03 +0100

    change inbuilt criminals to be stored as not inbuilt, and set inbuilt during __init__.py

--- BB/bbConfig/__init__.py
+++ BB/bbConfig/__init__.py
@@ -5,6 +5,7 @@ from ..bbObjects import bbCriminal, bbSystem
 
 for criminalDict in bbData.builtInCriminalData.values():
     bbData.builtInCriminalObjs[criminalDict["name"]] = bbCriminal.fromDict(criminalDict)
+    bbData.builtInCriminalObjs[criminalDict["name"]].builtIn = True
 
 for systemDict in bbData.builtInSystemData.values():
     bbData.builtInSystemObjs[systemDict["name"]] = bbSystem.fromDict(systemDict)
