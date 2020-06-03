a0ea775aa3ad4dedfe1a3be4fb5f9de84a5a5d77
Caperr <1jasperlaw@gmail.com>
2020-06-03 19:54:33 +0100

    add hasAnnounceChannel and hasPlayChannel

--- BB/bbObjects/bbGuild.py
+++ BB/bbObjects/bbGuild.py
@@ -3,6 +3,7 @@ class bbGuild:
     announceChannel = -1
     playChannel = -1
 
+
     def __init__(self, id, announceChannel=-1, playChannel=-1):
         self.id = id
         self.announceChannel = announceChannel
@@ -10,10 +11,14 @@ class bbGuild:
 
 
     def getAnnounceChannelId(self):
+        if not self.hasAnnounceChannel():
+            raise ValueError("This guild has no announce channel set")
         return self.announceChannel
 
 
     def getPlayChannelId(self):
+        if not self.hasPlayChannel():
+            raise ValueError("This guild has no play channel set")
         return self.playChannel
 
 
@@ -24,6 +29,14 @@ class bbGuild:
     def setPlayChannelId(self, playChannelId):
         self.playChannel = playChannelId
     
+
+    def hasAnnounceChannel(self):
+        return self.announceChannel != -1
+
+
+    def hasPlayChannel(self):
+        return self.playChannel != -1
+
 
     def toDictNoId(self):
         return {"announceChannel":self.announceChannel, "playChannel":self.playChannel}
