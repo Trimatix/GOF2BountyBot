c26da8a74dcf878db48540a78ba39b4f49d8476e
Trimatix <1jasperlaw@gmail.com>
2020-06-06 01:10:52 +0100

    add attributes for ship attribute modifications

--- BB/bbObjects/items/bbModule.py
+++ BB/bbObjects/items/bbModule.py
@@ -1,7 +1,22 @@
 from .. import bbAliasable
 
 class bbModule(bbAliasable.Aliasable):
+    armour = 0
+    armourMultiplier = 1.0
+
+    shield = 0
+    shieldMultiplier = 1.0
+
+    dmg = 0
+    dmgMultiplier = 1.0
+
+    cargo = 0
+    cargoMultiplier = 1.0
+
+    handling = 0
+    handlingMultiplier = 0
+
+    value = 0
 
     def __init__(self, name, aliases):
         super(bbModule, self).__init__(name, aliases)
-        
