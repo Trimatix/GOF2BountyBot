4353f7557b3ea7c22c6725a9715e440ec44ba7d6
Trimatix <1jasperlaw@gmail.com>
2020-06-06 01:24:36 +0100

    add ship modification attributes and constructor

--- BB/bbObjects/items/bbShipUpgrade.py
+++ BB/bbObjects/items/bbShipUpgrade.py
@@ -1,6 +1,59 @@
 class bbShipUpgrade:
     name = ""
     value = 0
+    applied = False
+
+    armour = 0.0
+    armourMultiplier = 1.0
+
+    cargo = 0
+    cargoMultiplier = 1.0
+
+    numSecondaries = 0
+    numSecondariesMultiplier = 1.0
+
+    handling = 0
+    handlingMultiplier = 1.0
+
+    maxPrimaries = 0
+    maxPrimariesMultiplier = 1.0
+    
+    maxTurrets = 0
+    maxTurretsMultiplier = 1.0
+
+    maxModules = 0
+    maxModulesMultiplier = 1.0
+
+    
+    def __init__(self, name, value,
+                    armour=0.0, armourMultiplier=1.0, cargo=0, cargoMultiplier=1.0, numSecondaries=0, numSecondariesMultiplier=1.0,
+                    handling=0, handlingMultiplier=1.0, maxPrimaries=0, maxPrimariesMultiplier=1.0, maxTurrets=0, maxTurretsMultiplier=1.0,
+                    maxModules=0, maxModulesMultiplier=1.0):
+        self.name = name
+        self.value = value
+
+        self.armour = armour
+        self.armourMultiplier = armourMultiplier
+
+        self.cargo = cargo
+        self.cargoMultiplier = cargoMultiplier
+
+        self.numSecondaries = numSecondaries
+        self.numSecondariesMultiplier = numSecondariesMultiplier
+
+        self.handling = handling
+        self.handlingMultiplier = handlingMultiplier
+
+        self.maxPrimaries = maxPrimaries
+        self.maxPrimariesMultiplier = maxPrimariesMultiplier
+        
+        self.maxTurrets = maxTurrets
+        self.maxTurretsMultiplier = maxTurretsMultiplier
+
+        self.maxModules = maxModules
+        self.maxModulesMultiplier = maxModulesMultiplier
+
+
 
     def __eq__(self, other):
         return type(self) == type(other) and self.name == other.name
