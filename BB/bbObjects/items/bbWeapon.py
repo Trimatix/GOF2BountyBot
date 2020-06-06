18c3ba99b26603ed9eeadc1e0b193fc35fe24742
Trimatix <1jasperlaw@gmail.com>
2020-06-06 01:11:03 +0100

    add value

--- BB/bbObjects/items/bbWeapon.py
+++ BB/bbObjects/items/bbWeapon.py
@@ -2,6 +2,7 @@ from .. import bbAliasable
 
 class bbWeapon(bbAliasable.Aliasable):
     dps = 0.0
+    value = 0
 
     def __init__(self, name, aliases):
         super(bbWeapon, self).__init__(name, aliases)
