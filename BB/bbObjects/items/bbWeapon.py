bba8418689067b7571c6606e5731ede7f6502138
Trimatix <1jasperlaw@gmail.com>
2020-06-09 00:34:32 +0100

    add wiki. fromDict now returns the builtIn object upon request.

--- BB/bbObjects/items/bbWeapon.py
+++ BB/bbObjects/items/bbWeapon.py
@@ -1,10 +1,11 @@
 from .. import bbAliasable
+from ...bbConfig import bbData
 
 class bbWeapon(bbAliasable.Aliasable):
     dps = 0.0
     value = 0
 
-    def __init__(self, name, aliases, dps=0.0, value=0):
+    def __init__(self, name, aliases, dps=0.0, value=0, wiki=""):
         super(bbWeapon, self).__init__(name, aliases)
 
 
@@ -13,4 +14,7 @@ class bbWeapon(bbAliasable.Aliasable):
 
 
 def fromDict(weaponDict):
-    return bbWeapon(weaponDict["name"], weaponDict["aliases"], dps=weaponDict["dps"], value=weaponDict["value"])
+    if weaponDict["builtIn"]:
+        return bbData.builtInWeaponObjs[weaponDict["name"]]
+    else:
+        return bbWeapon(weaponDict["name"], weaponDict["aliases"], dps=weaponDict["dps"], value=weaponDict["value"], wiki=weaponDict["wiki"] if "wiki" in weaponDict else "")
