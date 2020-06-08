f8802f567e4d0344e7770715657b435e514ea7ab
Trimatix <1jasperlaw@gmail.com>
2020-06-09 00:33:56 +0100

    Instead of a fixed int value, use a percentage of the ship in question's value for the upgrade's value - as in GOF2. add valueForShip to calculate this value for a given ship. change fromDict to return the builtIn object if one is requested, since shipUpgrades are now pseudo-constant and shared.

--- BB/bbObjects/items/bbShipUpgrade.py
+++ BB/bbObjects/items/bbShipUpgrade.py
@@ -1,7 +1,12 @@
+from ...bbConfig import bbData
+
 class bbShipUpgrade:
     name = ""
     value = 0
+    shipToUpgradeValueMult = 1.0
     applied = False
+    vendor = ""
+    hasVendor = False
 
     armour = 0.0
     armourMultiplier = 1.0
@@ -25,12 +30,14 @@ class bbShipUpgrade:
     maxModulesMultiplier = 1.0
 
     
-    def __init__(self, name, value,
+    def __init__(self, name, shipToUpgradeValueMult,
                     armour=0.0, armourMultiplier=1.0, cargo=0, cargoMultiplier=1.0, numSecondaries=0, numSecondariesMultiplier=1.0,
                     handling=0, handlingMultiplier=1.0, maxPrimaries=0, maxPrimariesMultiplier=1.0, maxTurrets=0, maxTurretsMultiplier=1.0,
-                    maxModules=0, maxModulesMultiplier=1.0):
+                    maxModules=0, maxModulesMultiplier=1.0, vendor=""):
         self.name = name
-        self.value = value
+        self.shipToUpgradeValueMult = shipToUpgradeValueMult
+        self.vendor = vendor
+        self.hasVendor = vendor != ""
 
         self.armour = armour
         self.armourMultiplier = armourMultiplier
@@ -58,19 +65,27 @@ class bbShipUpgrade:
     def __eq__(self, other):
         return type(self) == type(other) and self.name == other.name
 
+    
+    def valueForShip(self, ship):
+        return ship.value * self.shipToUpgradeValueMult
+
 
 def fromDict(upgradeDict):
-    return bbShipUpgrade(upgradeDict["name"], upgradeDict["value"], armour=upgradeDict["armour"] if "armour" in upgradeDict else 0.0,
-                            armourMultiplier=upgradeDict["armourMultiplier"] if "armourMultiplier" in upgradeDict else 1.0,
-                            cargo=upgradeDict["cargo"] if "cargo" in upgradeDict else 0,
-                            cargoMultiplier=upgradeDict["cargoMultiplier"] if "cargoMultiplier" in upgradeDict else 1.0,
-                            numSecondaries=upgradeDict["numSecondaries"] if "numSecondaries" in upgradeDict else 0,
-                            numSecondariesMultiplier=upgradeDict["numSecondariesMultiplier"] if "numSecondariesMultiplier" in upgradeDict else 1.0,
-                            handling=upgradeDict["handling"] if "handling" in upgradeDict else 0,
-                            handlingMultiplier=upgradeDict["handlingMultiplier"] if "handlingMultiplier" in upgradeDict else 1.0,
-                            maxPrimaries=upgradeDict["maxPrimaries"] if "maxPrimaries" in upgradeDict else 0,
-                            maxPrimariesMultiplier=upgradeDict["maxPrimariesMultiplier"] if "maxPrimariesMultiplier" in upgradeDict else 1.0,
-                            maxTurrets=upgradeDict["maxTurrets"] if "maxTurrets" in upgradeDict else 0,
-                            maxTurretsMultiplier=upgradeDict["maxTurretsMultiplier"] if "maxTurretsMultiplier" in upgradeDict else 1.0,
-                            maxModules=upgradeDict["maxModules"] if "maxModules" in upgradeDict else 0,
-                            maxModulesMultiplier=upgradeDict["maxModulesMultiplier"] if "maxModulesMultiplier" in upgradeDict else 1.0)
+    if upgradeDict["builtIn"]:
+        return bbData.builtInUpgradeObjs[upgradeDict["name"]]
+    else:
+        return bbShipUpgrade(upgradeDict["name"], upgradeDict["shipToUpgradeValueMult"], armour=upgradeDict["armour"] if "armour" in upgradeDict else 0.0,
+                                armourMultiplier=upgradeDict["armourMultiplier"] if "armourMultiplier" in upgradeDict else 1.0,
+                                cargo=upgradeDict["cargo"] if "cargo" in upgradeDict else 0,
+                                cargoMultiplier=upgradeDict["cargoMultiplier"] if "cargoMultiplier" in upgradeDict else 1.0,
+                                numSecondaries=upgradeDict["numSecondaries"] if "numSecondaries" in upgradeDict else 0,
+                                numSecondariesMultiplier=upgradeDict["numSecondariesMultiplier"] if "numSecondariesMultiplier" in upgradeDict else 1.0,
+                                handling=upgradeDict["handling"] if "handling" in upgradeDict else 0,
+                                handlingMultiplier=upgradeDict["handlingMultiplier"] if "handlingMultiplier" in upgradeDict else 1.0,
+                                maxPrimaries=upgradeDict["maxPrimaries"] if "maxPrimaries" in upgradeDict else 0,
+                                maxPrimariesMultiplier=upgradeDict["maxPrimariesMultiplier"] if "maxPrimariesMultiplier" in upgradeDict else 1.0,
+                                maxTurrets=upgradeDict["maxTurrets"] if "maxTurrets" in upgradeDict else 0,
+                                maxTurretsMultiplier=upgradeDict["maxTurretsMultiplier"] if "maxTurretsMultiplier" in upgradeDict else 1.0,
+                                maxModules=upgradeDict["maxModules"] if "maxModules" in upgradeDict else 0,
+                                maxModulesMultiplier=upgradeDict["maxModulesMultiplier"] if "maxModulesMultiplier" in upgradeDict else 1.0,
+                                vendor=upgradeDict["vendor"] if "vendor" in upgradeDict else "")
