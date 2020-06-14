2341f72082ac96582d55ce69f5f8ed0d104374d6
Trimatix <1jasperlaw@gmail.com>
2020-06-14 20:50:43 +0100

    change shopDefaultModulesNum to 5

--- BB/bbConfig/bbConfig.py
+++ BB/bbConfig/bbConfig.py
@@ -69,7 +69,7 @@ numTurretRanks = 3
 # The default number of items shops should generate every shopRefreshStockPeriod
 shopDefaultShipsNum = 5
 shopDefaultWeaponsNum = 5
-shopDefaultModulesNum = 3
+shopDefaultModulesNum = 5
 shopDefaultTurretsNum = 2
 
 # bbTurret is the only item that has a probability not to be spawned. This metric indicates the percentage chance of turrets being stocked on a given day
