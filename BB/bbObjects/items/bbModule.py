e29623eeaa125f1aed762eaf69daf37c1aa06228
Trimatix <1jasperlaw@gmail.com>
2020-06-16 14:31:08 +0100

    add CloakModule special class for cloaks. Also adapt bbmodule.fromdict to call different constructors if a module type constructor is registered.

--- BB/bbObjects/items/bbModule.py
+++ BB/bbObjects/items/bbModule.py
@@ -83,50 +83,60 @@ class bbModule(bbAliasable.Aliasable):
 
 
     def toDict(self):
-        # data = {"name": self.name, "aliases": self.aliases}
-        # if self.armour != 0:
-        #     data["armour"] = self.armour
-
-        # if self.armourMultiplier != 1.0:
-        #     data["armourMultiplier"] = self.armourMultiplier
-
-        # if self.shield != 0:
-        #     data["shield"] = self.shield
-
-        # if self.shieldMultiplier != 1.0:
-        #     data["shieldMultiplier"] = self.shieldMultiplier
-
-        # if self.dps != 0:
-        #     data["dps"] = self.dps
-
-        # if self.dpsMultiplier != 1.0:
-        #     data["dpsMultiplier"] = self.dpsMultiplier
-
-        # if self.cargo != 0:
-        #     data["cargo"] = self.cargo
-
-        # if self.cargoMultiplier != 1.0:
-        #     data["cargoMultiplier"] = self.cargoMultiplier
-
-        # if self.handling != 0:
-        #     data["handling"] = self.handling
-
-        # if self.handlingMultiplier != 1.0:
-        #     data["handlingMultiplier"] = self.handlingMultiplier
-
-        # return data
-
         return {"name": self.name, "builtIn": True}
+
+
+class CloakModule(bbModule):
+    duration = 0
+
+    value = 0
+    shopSpawnRate = 0
+
+    def __init__(self, name, aliases, duration=0, value=0, wiki="", manufacturer="", icon=""):
+        super(CloakModule, self).__init__(name, aliases, armour=0, armourMultiplier=1.0, shield=0, shieldMultiplier=1.0, dps=0,
+                    dpsMultiplier=1.0, cargo=0, cargoMultiplier=1.0, handling=0, handlingMultiplier=1.0, value=value, wiki=wiki, manufacturer=manufacturer, icon=icon)
+        self.duration = duration
+
+    
+    def getValue(self):
+        return self.value
+
+    
+    def statsStringShort(self):
+        return "Duration: " + str(self.duration) + "s" if self.duration != 0 else "No effect"
+
+    
+    def getType(self):
+        return CloakModule
+
+
+    def toDict(self):
+        return {"name": self.name, "builtIn": True}
+
+
+def cloakFromDict(moduleDict):
+    if "builtIn" in moduleDict and moduleDict["builtIn"]:
+        return bbData.builtInModuleObjs[moduleDict["name"]]
+    else:
+        return CloakModule(moduleDict["name"], moduleDict["aliases"] if "aliases" in moduleDict else [], value=moduleDict["value"] if "value" in moduleDict else 0,
+                            wiki=moduleDict["wiki"] if "wiki" in moduleDict else "", manufacturer=moduleDict["manufacturer"] if "manufacturer" in moduleDict else "", icon=moduleDict["icon"] if "icon" in moduleDict else bbData.rocketIcon,
+                            duration=moduleDict["duration"] if "duration" in moduleDict else 0)
+
+
+typeConstructors = {"cloak":cloakFromDict}
 
 
 def fromDict(moduleDict):
     if "builtIn" in moduleDict and moduleDict["builtIn"]:
         return bbData.builtInModuleObjs[moduleDict["name"]]
     else:
-        return bbModule(moduleDict["name"], moduleDict["aliases"] if "aliases" in moduleDict else [], armour=moduleDict["armour"] if "armour" in moduleDict else 0,
-                        armourMultiplier=moduleDict["armourMultiplier"] if "armourMultiplier" in moduleDict else 1, shield=moduleDict["shield"] if "shield" in moduleDict else 0,
-                        shieldMultiplier=moduleDict["shieldMultiplier"] if "shieldMultiplier" in moduleDict else 1, dps=moduleDict["dps"] if "dps" in moduleDict else 0,
-                        dpsMultiplier=moduleDict["dpsMultiplier"] if "dpsMultiplier" in moduleDict else 1, cargo=moduleDict["cargo"] if "cargo" in moduleDict else 0,
-                        cargoMultiplier=moduleDict["cargoMultiplier"] if "cargoMultiplier" in moduleDict else 1, handling=moduleDict["handling"] if "handling" in moduleDict else 0,
-                        handlingMultiplier=moduleDict["handlingMultiplier"] if "handlingMultiplier" in moduleDict else 1, value=moduleDict["value"] if "value" in moduleDict else 0,
-                        wiki=moduleDict["wiki"] if "wiki" in moduleDict else "", manufacturer=moduleDict["manufacturer"] if "manufacturer" in moduleDict else "", icon=moduleDict["icon"] if "icon" in moduleDict else bbData.rocketIcon)
+        if "type" in moduleDict and moduleDict["type"] in typeConstructors:
+            return typeConstructors[moduleDict["type"]](moduleDict)
+        else:
+            return bbModule(moduleDict["name"], moduleDict["aliases"] if "aliases" in moduleDict else [], armour=moduleDict["armour"] if "armour" in moduleDict else 0,
+                            armourMultiplier=moduleDict["armourMultiplier"] if "armourMultiplier" in moduleDict else 1, shield=moduleDict["shield"] if "shield" in moduleDict else 0,
+                            shieldMultiplier=moduleDict["shieldMultiplier"] if "shieldMultiplier" in moduleDict else 1, dps=moduleDict["dps"] if "dps" in moduleDict else 0,
+                            dpsMultiplier=moduleDict["dpsMultiplier"] if "dpsMultiplier" in moduleDict else 1, cargo=moduleDict["cargo"] if "cargo" in moduleDict else 0,
+                            cargoMultiplier=moduleDict["cargoMultiplier"] if "cargoMultiplier" in moduleDict else 1, handling=moduleDict["handling"] if "handling" in moduleDict else 0,
+                            handlingMultiplier=moduleDict["handlingMultiplier"] if "handlingMultiplier" in moduleDict else 1, value=moduleDict["value"] if "value" in moduleDict else 0,
+                            wiki=moduleDict["wiki"] if "wiki" in moduleDict else "", manufacturer=moduleDict["manufacturer"] if "manufacturer" in moduleDict else "", icon=moduleDict["icon"] if "icon" in moduleDict else bbData.rocketIcon)
