38f8fc19e4d5827ecbc3d491721380991662c077
Trimatix <1jasperlaw@gmail.com>
2020-06-28 21:10:50 +0100

    fix erroneous bbModule super constructor call

--- BB/bbObjects/items/modules/bbModule.py
+++ BB/bbObjects/items/modules/bbModule.py
@@ -4,7 +4,7 @@ from ....bbConfig import bbData
 class bbModule(bbItem):
     def __init__(self, name, aliases, armour=0, armourMultiplier=1.0, shield=0, shieldMultiplier=1.0, dps=0,
                     dpsMultiplier=1.0, cargo=0, cargoMultiplier=1.0, handling=0, handlingMultiplier=1.0, value=0, wiki="", manufacturer="", icon="", emoji=""):
-        super(bbModule, self).__init__(name, aliases, armour, value, wiki, manufacturer, icon, emoji)
+        super(bbModule, self).__init__(name, aliases, value=value, wiki=wiki, manufacturer=manufacturer, icon=icon, emoji=emoji)
         self.armour = armour
         self.armourMultiplier = armourMultiplier
         self.shield = shield
