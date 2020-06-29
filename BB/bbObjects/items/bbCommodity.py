3aa7460e2697bb24ae6295f85eea9e6753ca8165
Trimatix <1jasperlaw@gmail.com>
2020-06-29 22:12:09 +0100

    inherit bbCommodity from bbItem

--- BB/bbObjects/items/bbCommodity.py
+++ BB/bbObjects/items/bbCommodity.py
@@ -1,34 +1,15 @@
-from .. import bbAliasable
+from . import bbItem
 from ...bbConfig import bbData
 
 
-class bbCommodity(bbAliasable.Aliasable):
-    value = 0
-    wiki = ""
-    hasWiki = False
-    icon = ""
-    hasIcon = False
-    emoji = ""
-    hasEmoji = False
-
+class bbCommodity(bbItem.bbItem):
     def __init__(self, name, aliases, value=0, wiki="", icon="", emoji=""):
-        super(bbCommodity, self).__init__(name, aliases)
-        self.value = value
-        self.wiki = wiki
-        self.hasWiki = wiki != ""
-        self.icon = ""
-        self.hasIcon = icon != ""
-        self.emoji = emoji
-        self.hasEmoji = emoji != ""
+        super(bbCommodity, self).__init__(name, aliases, value=value, wiki=wiki, icon=icon, emoji=emoji)
 
-    def getValue(self):
-        return self.value
 
     def getType(self):
         return bbCommodity
 
-    def toDict(self):
-        return {"name": self.name, "builtIn": True}
 
 def fromDict(commodityDict):
     if commodityDict["builtIn"]:
