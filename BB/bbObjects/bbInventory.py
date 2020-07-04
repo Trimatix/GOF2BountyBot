c1f981763d9a701eac6d31ea45cabcaf9826df52
Trimatix <1jasperlaw@gmail.com>
2020-07-04 18:54:16 +0100

    remove temporary bbInventoryListing copy. Fix bbInventory.getPage bugs in choosing item indices

--- BB/bbObjects/bbInventory.py
+++ BB/bbObjects/bbInventory.py
@@ -11,7 +11,7 @@ class bbInventory:
         if item in self.items:
             self.items[item].count += 1
         else:
-            self.items[item] = quantity
+            self.items[item] = bbInventoryListing(item, quantity)
             self.keys.append(item)
             self.numKeys += 1
 
@@ -38,35 +38,7 @@ class bbInventory:
         
         pageIndex = pageNum - 1
         page = {}
-        for item in self.keys[pageIndex * itemsPerPage + 1: max(pageNum * itemsPerPage, self.numKeys)]:
+        for item in self.keys[pageIndex * itemsPerPage: min(pageNum * itemsPerPage, self.numKeys)]:
             page[item] = self.items[item]
 
         return page
-
-
-class bbInventoryListing():
-    def __init__(self, item, count=0):
-        self.item = item
-        self.count = count
-
-    def increaseCount(self, numIncrease):
-        self.count += numIncrease
-
-    def decreaseCount(self, numDecrease):
-        if self.count < numDecrease:
-            raise ValueError("INVLIS_DECRCOUNT_NEG: Attempted to decreaseCount into a negative total: " + str(self.count) + " - " + str(numDecrease))
-        self.count -= numDecrease
-
-    def getItem(self):
-        return self.item
-
-    def storesItem(self, otherItem):
-        return self.item is otherItem
-
-    def statsStringShort(self):
-        return str(self.count) + " in inventory. " + str(self.item.value) + " credits each"
-
-    def toDict(self):
-        return {"item": self.item.toDict(), "count": self.count}
-
-
