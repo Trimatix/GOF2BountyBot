fb4b27e1e78ee85c7852d3db35cd76a5cf8f7fbc
Trimatix <1jasperlaw@gmail.com>
2020-07-06 20:37:52 +0100

    sort game objects into tech levels in bbData.[object]ObjsByTL lists

--- BB/bbConfig/__init__.py
+++ BB/bbConfig/__init__.py
@@ -58,106 +58,23 @@ for turretDict in bbData.builtInTurretData.values():
     bbData.builtInTurretData[turretDict["name"]]["builtIn"] = True
     bbData.builtInTurretObjs[turretDict["name"]].builtIn = True
 
+bbData.shipKeysByTL = [[] for currentTL in range(bbConfig.minTechLevel, bbConfig.maxTechLevel + 1)]
 
-# Sort ships by value
-unsortedShipKeys = {}
-for shipKey in bbData.builtInShipData.keys():
-    unsortedShipKeys[shipKey] = bbData.builtInShipData[shipKey]["value"]
-sortedShipKeys = sorted(unsortedShipKeys.items(), key=operator.itemgetter(1))[::-1]
+for currentShipKey in bbData.builtInShipData.keys():
+    bbData.shipKeysByTL[bbData.builtInShipData[currentShipKey]["techLevel"] - 1].append(currentShipKey)
 
-shipKeysNum = {}
-for rank in range(bbConfig.numShipRanks):
-    bbData.shipRankIndexFreqs[rank] = 0
-# Make a list of ship KEYS ranked by value for random picking
-for keyIndex in range(len(sortedShipKeys) - 1, -1, -1):
-    # currentShip = bbData.builtInShipObjs[sortedShipKeys[keyIndex][0]]
-    currentShip = sortedShipKeys[keyIndex][0]
-    shipKeysNum[currentShip] = int(keyIndex / bbConfig.numShipRanks)
-    itemRank = int(keyIndex / bbConfig.numShipRanks)
-    bbData.shipRankIndexFreqs[itemRank] += 1
-    for n in range(itemRank):
-        bbData.rankedShipKeys.append(currentShip)
-print("ship rank freqs: ",end="")
-print(bbData.shipRankIndexFreqs)
+bbData.moduleObjsByTL = [[] for currentTL in range(bbConfig.minTechLevel, bbConfig.maxTechLevel + 1)]
+for currentModuleObj in bbData.builtInModuleObjs.values():
+    bbData.moduleObjsByTL[currentModuleObj.techLevel - 1].append(currentModuleObj)
 
-numRankedShipKeys = len(bbData.rankedShipKeys)
-for shipKey in bbData.builtInShipData.keys():
-    bbData.shipKeySpawnRates[shipKey] = round((shipKeysNum[shipKey] / numRankedShipKeys) * 100, 2)
+bbData.weaponObjsByTL = [[] for currentTL in range(bbConfig.minTechLevel, bbConfig.maxTechLevel + 1)]
+for currentWeaponObj in bbData.builtInWeaponObjs.values():
+    bbData.weaponObjsByTL[currentWeaponObj.techLevel - 1].append(currentWeaponObj)
 
+bbData.turretObjsByTL = [[] for currentTL in range(bbConfig.minTechLevel, bbConfig.maxTechLevel + 1)]
+for currentTurretObj in bbData.builtInTurretObjs.values():
+    bbData.turretObjsByTL[currentTurretObj.techLevel - 1].append(currentTurretObj)
 
-# Sort weapons by value
-unsortedWeaponKeys = {}
-for weaponKey in bbData.builtInWeaponObjs.keys():
-    unsortedWeaponKeys[weaponKey] = bbData.builtInWeaponObjs[weaponKey].value
-sortedWeaponKeys = sorted(unsortedWeaponKeys.items(), key=operator.itemgetter(1))[::-1]
-
-weaponKeysNum = {}
-for rank in range(bbConfig.numWeaponRanks):
-    bbData.weaponRankIndexFreqs[rank] = 0
-# Make a list of weapons ranked by value for random picking
-for keyIndex in range(len(sortedWeaponKeys) - 1, -1, -1):
-    currentWeapon = bbData.builtInWeaponObjs[sortedWeaponKeys[keyIndex][0]]
-    weaponKeysNum[sortedWeaponKeys[keyIndex][0]] = int(keyIndex / bbConfig.numWeaponRanks)
-    itemRank = int(keyIndex / bbConfig.numWeaponRanks)
-    bbData.weaponRankIndexFreqs[itemRank] += 1
-    for n in range(itemRank):
-        bbData.rankedWeaponObjs.append(currentWeapon)
-print("weapon rank freqs: ",end="")
-print(bbData.weaponRankIndexFreqs)
-
-numRankedWeaponObjs = len(bbData.builtInWeaponObjs)
-
-for weapon in bbData.builtInWeaponObjs.values():
-    weapon.shopSpawnRate = round((weaponKeysNum[weapon.name] / numRankedWeaponObjs) * 100, 2)
-
-
-# Sort modules by value
-unsortedModuleKeys = {}
-for moduleKey in bbData.builtInModuleObjs.keys():
-    unsortedModuleKeys[moduleKey] = bbData.builtInModuleObjs[moduleKey].value
-sortedModuleKeys = sorted(unsortedModuleKeys.items(), key=operator.itemgetter(1))[::-1]
-
-moduleKeysNum = {}
-for rank in range(bbConfig.numModuleRanks):
-    bbData.moduleRankIndexFreqs[rank] = 0
-# Make a list of modules ranked by value for random picking
-for keyIndex in range(len(sortedModuleKeys) - 1, -1, -1):
-    currentModule = bbData.builtInModuleObjs[sortedModuleKeys[keyIndex][0]]
-    moduleKeysNum[sortedModuleKeys[keyIndex][0]] = int(keyIndex / bbConfig.numModuleRanks)
-    itemRank = int(keyIndex / bbConfig.numModuleRanks)
-    bbData.moduleRankIndexFreqs[itemRank] += 1
-    for n in range(itemRank):
-        bbData.rankedModuleObjs.append(currentModule)
-print("module rank freqs: ",end="")
-print(bbData.moduleRankIndexFreqs)
-
-numRankedModuleObjs = len(bbData.builtInModuleObjs)
-
-for module in bbData.builtInModuleObjs.values():
-    module.shopSpawnRate = round((moduleKeysNum[module.name] / numRankedModuleObjs) * 100, 2)
-
-
-# Sort turrets by value
-unsortedTurretKeys = {}
-for turretKey in bbData.builtInTurretObjs.keys():
-    unsortedTurretKeys[turretKey] = bbData.builtInTurretObjs[turretKey].value
-sortedTurretKeys = sorted(unsortedTurretKeys.items(), key=operator.itemgetter(1))[::-1]
-
-turretKeysNum = {}
-for rank in range(bbConfig.numTurretRanks):
-    bbData.turretRankIndexFreqs[rank] = 0
-# Make a list of turrets ranked by value for random picking
-for keyIndex in range(len(sortedTurretKeys) - 1, -1, -1):
-    currentTurret = bbData.builtInTurretObjs[sortedTurretKeys[keyIndex][0]]
-    turretKeysNum[sortedTurretKeys[keyIndex][0]] = int(keyIndex / bbConfig.numTurretRanks)
-    itemRank = int(keyIndex / bbConfig.numTurretRanks)
-    bbData.turretRankIndexFreqs[itemRank] += 1
-    for n in range(itemRank):
-        bbData.rankedTurretObjs.append(currentTurret)
-print("turret rank freqs: ",end="")
-print(bbData.turretRankIndexFreqs)
-
-numRankedTurretObjs = len(bbData.builtInTurretObjs)
 
 for turret in bbData.builtInTurretObjs.values():
     turret.shopSpawnRate = round(((turretKeysNum[turret.name] / numRankedTurretObjs) * 100) * (bbConfig.turretSpawnProbability / 100), 2)
