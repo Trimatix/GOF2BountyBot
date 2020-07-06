1d91bf3eff2bc00b8ea18a284079fb4e4bfa5c5f
Trimatix <1jasperlaw@gmail.com>
2020-07-06 20:46:55 +0100

    add techLevel to bbTurret constructor

--- BB/bbObjects/items/bbTurret.py
+++ BB/bbObjects/items/bbTurret.py
@@ -4,8 +4,8 @@ from ...bbConfig import bbData
 class bbTurret(bbItem):
     dps = 0.0
 
-    def __init__(self, name, aliases, dps=0.0, value=0, wiki="", manufacturer="", icon="", emoji=""):
-        super(bbTurret, self).__init__(name, aliases, value=value, wiki=wiki, manufacturer=manufacturer, icon=icon, emoji=emoji)
+    def __init__(self, name, aliases, dps=0.0, value=0, wiki="", manufacturer="", icon="", emoji="", techLevel=-1):
+        super(bbTurret, self).__init__(name, aliases, value=value, wiki=wiki, manufacturer=manufacturer, icon=icon, emoji=emoji, techLevel=techLevel)
 
         self.dps = dps
 
