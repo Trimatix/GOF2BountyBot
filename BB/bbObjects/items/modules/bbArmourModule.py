5e6b7aaac506360c4a3577f142b5ba8552c5ca74
Trimatix <1jasperlaw@gmail.com>
2020-07-06 15:05:37 +0100

    fix duplicate techLevel KWarg pass in bbArmourModule.fromDict

--- BB/bbObjects/items/modules/bbArmourModule.py
+++ BB/bbObjects/items/modules/bbArmourModule.py
@@ -14,4 +14,4 @@ def fromDict(moduleDict):
     return bbArmourModule(moduleDict["name"], moduleDict["aliases"] if "aliases" in moduleDict else [], armour=moduleDict["armour"] if "armour" in moduleDict else 0,
                             value=moduleDict["value"] if "value" in moduleDict else 0, wiki=moduleDict["wiki"] if "wiki" in moduleDict else "",
                             manufacturer=moduleDict["manufacturer"] if "manufacturer" in moduleDict else "", icon=moduleDict["icon"] if "icon" in moduleDict else bbData.rocketIcon,
-                            emoji=moduleDict["emoji"] if "emoji" in moduleDict else "", techLevel=moduleDict["techLevel"] if "techLevel" in moduleDict else -1, techLevel=moduleDict["techLevel"] if "techLevel" in moduleDict else -1)
+                            emoji=moduleDict["emoji"] if "emoji" in moduleDict else "", techLevel=moduleDict["techLevel"] if "techLevel" in moduleDict else -1)
