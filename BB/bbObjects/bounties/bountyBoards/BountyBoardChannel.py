fdfe931c675aa6a3667b87411a8ff66b7dae8921
Trimatix <1jasperlaw@gmail.com>
2020-07-19 12:10:31 +0100

    display criminal difficulty and loadout command in bounty listings

--- BB/bbObjects/bounties/bountyBoards/BountyBoardChannel.py
+++ BB/bbObjects/bounties/bountyBoards/BountyBoardChannel.py
@@ -17,11 +17,12 @@ def makeBountyEmbed(bounty):
         else:
             routeStr += system
         routeStr += ", "
-    embed.add_field(name="**Route:**", value=routeStr[:-2], inline=False)
-    embed.add_field(name="-", value="> ~~Already checked systems~~\n> Criminal spotted here recently**\***") #"‎"
     # embed.add_field(value="`Stars indicate systems where the criminal has recently been spotted.`", name="`Crossed-through systems have already been checked.`")
-    # embed.add_field(name="**Difficulty:**", value=str(bounty.criminal.techLevel))
-    # embed.add_field(name="**See the culprit's loadout with:**", value="`" + bbConfig.commandPrefix + "loadout criminal " + bounty.criminal.name + "`")
+    embed.add_field(name="**Difficulty:**", value=str(bounty.criminal.techLevel))
+    # embed = bbUtil.fillLoadoutEmbed(bounty.criminal.activeShip, embed, shipEmoji=True)
+    embed.add_field(name="**See the culprit's loadout with:**", value="`" + bbConfig.commandPrefix + "loadout criminal " + bounty.criminal.name + "`")
+    embed.add_field(name="**Route:**", value=routeStr[:-2], inline=False)
+    embed.add_field(name="-", value="> ~~Already checked systems~~\n> Criminal spotted here recently**\***")
     return embed
 
 
