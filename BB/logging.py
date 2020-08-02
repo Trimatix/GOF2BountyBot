6ca070c96007d616ae8e8ec209fe0ae5227190e7
Trimatix <1jasperlaw@gmail.com>
2020-08-03 00:57:51 +0100

    change logs storage from strings to dictionaries of datetimes to strings. No change to logger.log usage, except for the addition of an optional kwarg 'trace' which is omitted from console printing, but written to file during saving.

--- BB/logging.py
+++ BB/logging.py
@@ -5,20 +5,57 @@ from datetime import datetime
 
 class logger:
     def __init__(self):
-        self.logs = {"usersDB":"", "guildsDB":"", "bountiesDB":"",
-                        "shop":"", "escapedBounties": "", "bountyConfig": "", "duels": "",
-                        "hangar": "", "misc": "", "bountyBoards": ""}
+        self.clearLogs()
+
+
+    def clearLogs(self):
+        self.logs = {"usersDB":{}, "guildsDB":{}, "bountiesDB":{},
+                        "shop":{}, "escapedBounties": {}, "bountyConfig": {}, "duels": {},
+                        "hangar": {}, "misc": {}, "bountyBoards": {}}
+
+
+    def isEmpty(self):
+        for cat in self.logs:
+            if bool(self.logs[cat]):
+                return False
+        return True
+
+
+    def peekHeadTimeAndCategory(self):
+        head, headCat = None, ""
+        for cat in self.logs:
+            if bool(self.logs[cat]):
+                currHead = list(self.logs[cat].keys())[0]
+                if head is None or currHead < head:
+                    head, headCat = currHead, cat
+
+        return head, headCat
+
+
+    def popHeadLogAndCategory(self):
+        head, headCat = self.peekHeadTimeAndCategory()
+
+        if head is None:
+            log = ""
+        else:
+            log = self.logs[headCat][head]
+            del self.logs[headCat][head]
+
+        return log, headCat
 
 
     def save(self):
+        if self.isEmpty():
+            return
+
         logsSaved = ""
         files = {}
         nowStr = datetime.utcnow().strftime("(%d/%m/%H:%M)")
-
+        
         for category in self.logs:
-            if self.logs[category] != "":
+            if bool(self.logs[category]):
                 currentFName = bbConfig.loggingFolderPath + ("" if bbConfig.loggingFolderPath.endswith("/") else "/") + category + ".txt"
-                logsSaved += category + ".txt"
+                logsSaved += category + ".txt, "
 
                 if category not in files:
                     if not path.exists(currentFName):
@@ -35,25 +72,30 @@ class logger:
                         print(nowStr + "-[LOG::SAVE]>F_OPN_IOERR: ERROR OPENING LOG FILE: " + currentFName + "\n" + str(e))
                         files[category] = None
 
-                if files[category] is not None:
-                    try:
-                        files[category].write(self.logs[category])
-                        logsSaved += ", "
-                    except IOError as e:
-                        print(nowStr + "-[LOG::SAVE]>F_WRT_IOERR: ERROR WRITING TO LOG FILE: " + currentFName + "\n" + str(e))
+        while not self.isEmpty():
+            log, category = self.popHeadLogAndCategory()
+            if files[category] is not None:
+                try:
+                    files[category].write(log)
+                except IOError as e:
+                    print(nowStr + "-[LOG::SAVE]>F_WRT_IOERR: ERROR WRITING TO LOG FILE: " + currentFName + "\n" + str(e))
         
         for f in files.values():
             f.close()
         if logsSaved != "":
             print(nowStr + "-[LOG::SAVE]>SAVE_DONE: Logs saved: " + logsSaved[:-2])
+        
+        self.clearLogs()
 
 
-    def log(self, classStr, funcStr, event, category="misc", eventType="MISC_ERR"):
+    def log(self, classStr, funcStr, event, category="misc", eventType="MISC_ERR", trace=""):
         if category not in self.logs:
             self.log("misc", "Log", "log", "ATTEMPTED TO LOG TO AN UNKNOWN CATEGORY '" + str(category) + "' -> Redirected to misc.", eventType="UNKWN_CTGR")
-        eventStr = datetime.utcnow().strftime("(%d/%m/%H:%M)") + "-[" + str(classStr).upper() + "::" + str(funcStr).upper() + "]>" + str(eventType) + ": " + str(event)
+
+        now = datetime.utcnow()
+        eventStr = now.strftime("(%d/%m/%H:%M)") + "-[" + str(classStr).upper() + "::" + str(funcStr).upper() + "]>" + str(eventType) + ": " + str(event)
         print(eventStr)
-        self.logs[category] += eventStr + "\n\n"
+        self.logs[category][now] = eventStr + ("\n" + trace if trace != "" else "") + "\n\n"
 
 
 bbLogger = logger()
