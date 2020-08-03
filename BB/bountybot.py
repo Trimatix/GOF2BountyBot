adf6e72a3fe03aa6ab2a571b300c9ea7f7530ca6
Trimatix <1jasperlaw@gmail.com>
2020-08-03 15:48:38 +0100

    fix hangar debugging commands to work with erroneous items. add del-item-key

--- BB/bountybot.py
+++ BB/bountybot.py
@@ -3481,6 +3481,108 @@ dmCommands.register("del-item", dev_cmd_del_item)
 
 
 """
+Delete ALL of an item in a requested user's inventory.
+arg 1: user mention or ID
+arg 2: item type (ship/weapon/module/turret)
+arg 3: item number (from $hangar)
+
+@param message -- the discord message calling the command
+@param args -- string containing a user mention, an item type and an index number, separated by a single space
+"""
+async def dev_cmd_del_item_key(message, args, isDM):
+    argsSplit = args.split(" ")
+    if len(argsSplit) < 3:
+        await message.channel.send(":x: Not enough arguments! Please provide a user, an item type (ship/weapon/module/turret) and an item number from `" + bbConfig.commandPrefix + "hangar`")
+        return
+    if len(argsSplit) > 3:
+        await message.channel.send(":x: Too many arguments! Please only give a user, an item type (ship/weapon/module/turret), and an item number.")
+        return
+
+    item = argsSplit[1].rstrip("s")
+    if item == "all" or item not in bbConfig.validItemNames:
+        await message.channel.send(":x: Invalid item name! Please choose from: ship, weapon, module or turret.")
+        return
+
+    if not (bbUtil.isInt(argsSplit[0]) or bbUtil.isMention(argsSplit[0])):
+        await message.channel.send(":x: Invalid user! ")
+        return
+    requestedBBUser = usersDB.getOrAddID(int(argsSplit[0].lstrip("<@!").rstrip(">")))
+
+    requestedUser = client.get_user(requestedBBUser.id)
+    if requestedUser is None:
+        await message.channel.send(":x: Unrecognised user!")
+        return
+
+    itemNum = argsSplit[2]
+    if not bbUtil.isInt(itemNum):
+        await message.channel.send(":x: Invalid item number!")
+        return
+    itemNum = int(itemNum)
+
+    userItemInactives = requestedBBUser.getInactivesByName(item)
+    if itemNum > userItemInactives.numKeys:
+        await message.channel.send(":x: Invalid item number! The user only has " + str(userItemInactives.numKeys) + " " + item + "s.")
+        return
+    if itemNum < 1:
+        await message.channel.send(":x: Invalid item number! Must be at least 1.")
+        return
+
+    requestedItem = userItemInactives.keys[itemNum - 1]
+    itemName = ""
+    itemEmbed = None
+    
+    if item == "ship":
+        itemName = requestedItem.getNameAndNick()
+        itemEmbed = makeEmbed(col=bbData.factionColours[requestedItem.manufacturer] if requestedItem.manufacturer in bbData.factionColours else bbData.factionColours[
+                                    "neutral"], thumb=requestedItem.icon if requestedItem.hasIcon else "")
+
+        if requestedItem is None:
+            itemEmbed.add_field(name="Item:",
+                                    value="None", inline=False)
+        else:
+            itemEmbed.add_field(name="Item:", value=requestedItem.getNameAndNick(
+            ) + "\n" + requestedItem.statsStringNoItems(), inline=False)
+
+            if requestedItem.getMaxPrimaries() > 0:
+                itemEmbed.add_field(name="‎", value="__**Equipped Weapons**__ *" + str(len(
+                    requestedItem.weapons)) + "/" + str(requestedItem.getMaxPrimaries()) + "*", inline=False)
+                for weaponNum in range(1, len(requestedItem.weapons) + 1):
+                    itemEmbed.add_field(name=str(weaponNum) + ". " + requestedItem.weapons[weaponNum - 1].name, value=(
+                        requestedItem.weapons[weaponNum - 1].emoji if requestedItem.weapons[weaponNum - 1].hasEmoji else "") + requestedItem.weapons[weaponNum - 1].statsStringShort(), inline=True)
+
+            if requestedItem.getMaxModules() > 0:
+                itemEmbed.add_field(name="‎", value="__**Equipped Modules**__ *" + str(len(
+                    requestedItem.modules)) + "/" + str(requestedItem.getMaxModules()) + "*", inline=False)
+                for moduleNum in range(1, len(requestedItem.modules) + 1):
+                    itemEmbed.add_field(name=str(moduleNum) + ". " + requestedItem.modules[moduleNum - 1].name, value=(
+                        requestedItem.modules[moduleNum - 1].emoji if requestedItem.modules[moduleNum - 1].hasEmoji else "") + requestedItem.modules[moduleNum - 1].statsStringShort(), inline=True)
+
+            if requestedItem.getMaxTurrets() > 0:
+                itemEmbed.add_field(name="‎", value="__**Equipped Turrets**__ *" + str(len(
+                    requestedItem.turrets)) + "/" + str(requestedItem.getMaxTurrets()) + "*", inline=False)
+                for turretNum in range(1, len(requestedItem.turrets) + 1):
+                    itemEmbed.add_field(name=str(turretNum) + ". " + requestedItem.turrets[turretNum - 1].name, value=(
+                        requestedItem.turrets[turretNum - 1].emoji if requestedItem.turrets[turretNum - 1].hasEmoji else "") + requestedItem.turrets[turretNum - 1].statsStringShort(), inline=True)
+
+    else:
+        itemName = requestedItem.name + "\n" + requestedItem.statsStringShort()
+
+    if requestedItem not in userItemInactives.items:
+        userItemInactives.keys.remove(requestedItem)
+        userItemInactives.numKeys -= 1
+        await message.channel.send(":white_check_mark: **Erroneous key** deleted from " + userOrMemberName(requestedUser, message.guild) + "'s inventory: " + itemName, embed=itemEmbed)
+    else:
+        itemCount = userItemInactives.items[requestedItem].count
+        del userItemInactives.items[requestedItem]
+        userItemInactives.keys.remove(requestedItem)
+        userItemInactives.numKeys -= 1
+        await message.channel.send(":white_check_mark: " + str(itemCount) + " item(s) deleted from " + userOrMemberName(requestedUser, message.guild) + "'s inventory: " + itemName, embed=itemEmbed)
+
+bbCommands.register("del-item-key", dev_cmd_del_item_key)
+dmCommands.register("del-item-key", dev_cmd_del_item_key)
+
+
+"""
 developer command setting the checking cooldown applied to users
 this does not update bbConfig and will be reverted on bot restart
 
@@ -4193,18 +4295,41 @@ async def dev_cmd_debug_hangar(message, args, isDM):
 
         for itemType in itemTypes:
             itemInv = requestedBBUser.getInactivesByName(itemType)
+            displayedItems = []
+            await message.channel.send(itemType.upper() + " KEYS: " + str(itemInv.keys) + "\n" + itemType.upper() + " LISTINGS: " + str(list(itemInv.items.keys())))
 
             for itemNum in range(firstPlace, requestedBBUser.lastItemNumberOnPage(itemType, page, maxPerPage) + 1):
                 if itemNum == firstPlace:
                     hangarEmbed.add_field(name="‎", value="__**Stored " + itemType.title() + "s**__", inline=False)
-                currentItem = itemInv[itemNum - 1].item
-                currentItemCount = itemInv.numStored(currentItem)
+                currentItem = itemInv.keys[itemNum - 1]
+                itemStored = currentItem in itemInv.items
+                currentItemCount = itemInv.numStored(currentItem) if itemStored else 0
+                displayedItems.append(currentItem)
                 if itemType == "ship":
                     currentItemName = currentItem.getNameAndNick()
                 else:
                     currentItemName = currentItem.name
-                hangarEmbed.add_field(name=str(itemNum) + ". " + ((" `(" + str(currentItemCount) + ")` ") if currentItemCount > 1 else "") + repr(currentItem) + "\n" + currentItemName, 
-                                    value=(currentItem.emoji if currentItem.hasEmoji else "") + currentItem.statsStringShort(), inline=False)
+                try:
+                    hangarEmbed.add_field(name=str(itemNum) + ". " + ("" if itemStored else "⚠ KEY NOT FOUND IN ITEMS DICT ") + ((" `(" + str(currentItemCount) + ")` ") if currentItemCount > 1 else "") + currentItemName + "\n`" + repr(currentItem) + "`", 
+                                        value=(currentItem.emoji if currentItem.hasEmoji else "") + currentItem.statsStringShort(), inline=False)
+                except AttributeError:
+                    hangarEmbed.add_field(name=str(itemNum) + ". " + ("" if itemStored else "⚠ KEY NOT FOUND IN ITEMS DICT ") + ((" `(" + str(currentItemCount) + ")` ") if currentItemCount > 1 else "") + currentItemName + "\n`" + repr(currentItem) + "`", 
+                                        value="unexpected type", inline=False)
+
+            for itemKey in itemInv.items:
+                if itemKey not in displayedItems:
+                    currentItemCount = itemInv.items[itemKey].count
+                    displayedItems.append(itemKey)
+                    if itemType == "ship":
+                        currentItemName = itemKey.getNameAndNick()
+                    else:
+                        currentItemName = itemKey.name
+                    try:
+                        hangarEmbed.add_field(name=str(itemNum) + ". ⚠ ITEM LISTING NOT FOUND IN KEYS " + ((" `(" + str(currentItemCount) + ")` ") if currentItemCount > 1 else "") + currentItemName + "\n`" + repr(itemKey) + "`", 
+                                            value=(itemKey.emoji if itemKey.hasEmoji else "") + itemKey.statsStringShort(), inline=False)
+                    except AttributeError:
+                        hangarEmbed.add_field(name=str(itemNum) + ". ⚠ ITEM LISTING NOT FOUND IN KEYS " + ((" `(" + str(currentItemCount) + ")` ") if currentItemCount > 1 else "") + currentItemName + "\n`" + repr(itemKey) + "`", 
+                                                value="unexpected type", inline=False)
 
         await message.channel.send(embed=hangarEmbed)
 
