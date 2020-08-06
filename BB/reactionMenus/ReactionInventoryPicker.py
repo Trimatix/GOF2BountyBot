f9fb7f9c3ca80e90a5c664ff89b04cecaf7d201f
Trimatix <1jasperlaw@gmail.com>
2020-08-06 15:16:03 +0100

    add timeout, replace func with addFunc

--- BB/reactionMenus/ReactionInventoryPicker.py
+++ BB/reactionMenus/ReactionInventoryPicker.py
@@ -3,8 +3,8 @@ from ..bbConfig import bbConfig
 maxItemsPerPage = len(bbConfig.defaultMenuEmojis)
 
 
-class ReactionInventoryPicker(ReactionMenu.ReactionMenu):
-    def __init__(self, msg, bbInventory, itemsPerPage, titleTxt="", desc="", col=None, footerTxt="", img="", thumb="", icon="", authorName=""):
+class ReactionInventoryPicker(ReactionMenu.CancellableReactionMenu):
+    def __init__(self, msg, bbInventory, itemsPerPage, titleTxt="", desc="", col=None, footerTxt="", img="", thumb="", icon="", authorName="", timeout=None):
         if itemsPerPage > 10:
             raise ValueError("Tried to instantiate a ReactionItemPicker with more than " + str(maxItemsPerPage) + " itemsPerPage (requested " + str(itemsPerPage) + ")")
         self.bbInventory = bbInventory
@@ -16,9 +16,9 @@ class ReactionInventoryPicker(ReactionMenu.ReactionMenu):
         for itemNum in range(len(itemPage)):
             optionEmoji = bbConfig.defaultMenuEmojis[itemNum]
             item = itemPage[itemNum].item
-            itemOptions[optionEmoji] = ReactionMenu.ReactionMenuOption(optionEmoji, self.pickItem, item, item.name + ((" " + item.emoji) if item.hasEmoji else ""))
+            itemOptions[optionEmoji] = ReactionMenu.ReactionMenuOption(item.name + ((" " + item.emoji) if item.hasEmoji else ""), optionEmoji, addFunc=self.pickItem, addArgs=item)
 
-        super(ReactionInventoryPicker, self).__init__(msg, options=itemOptions, titleTxt=titleTxt, desc=desc, col=col, footerTxt=footerTxt, img=img, thumb=thumb, icon=icon, authorName=authorName)
+        super(ReactionInventoryPicker, self).__init__(msg, options=itemOptions, titleTxt=titleTxt, desc=desc, col=col, footerTxt=footerTxt, img=img, thumb=thumb, icon=icon, authorName=authorName, timeout=timeout)
 
 
     def pickItem(self, item):
