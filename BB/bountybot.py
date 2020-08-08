221f0afb6738714562c6a729b9d9847cb39739c3
Trimatix <1jasperlaw@gmail.com>
2020-08-08 21:22:09 +0100

    fix userOrMemberName reference in cmd_check

--- BB/bountybot.py
+++ BB/bountybot.py
@@ -1064,7 +1064,7 @@ async def cmd_check(message, args, isDM):
         if bountyWon:
             requestedBBUser.bountyWins += 1
             await message.channel.send(sightedCriminalsStr + "\n" + ":moneybag: **" + message.author.display_name + "**, you now have **" + str(requestedBBUser.credits) + " Credits!**" +
-                                       ("\nYou have now reached the maximum number of bounty wins allowed for today! Please check back tomorrow." if dailyBountiesMaxReached else userOrMemberName(message.author) + ", you have **" + str(bbConfig.maxDailyBountyWins - usersDB.getOrAddID(message.author.id).bountyWinsToday) + "** remaining bounty wins today!"))
+                                       ("\nYou have now reached the maximum number of bounty wins allowed for today! Please check back tomorrow." if dailyBountiesMaxReached else userOrMemberName(message.author, message.guild) + ", you have **" + str(bbConfig.maxDailyBountyWins - usersDB.getOrAddID(message.author.id).bountyWinsToday) + "** remaining bounty wins today!"))
 
             if sightedCriminalsStr != "":
                 for currentGuild in guildsDB.getGuilds():
