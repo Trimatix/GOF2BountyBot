f5fd12c4e39fbdd1f75373efe240bcd7617969ec
Trimatix <1jasperlaw@gmail.com>
2020-09-15 18:29:54 +0100

    convert from google style docstrings to reST style

--- BB/bbDatabases/HeirarchicalCommandsDB.py
+++ BB/bbDatabases/HeirarchicalCommandsDB.py
@@ -1,28 +1,29 @@
-"""
-Class that stores, categorises, and calls commands based on a text name and caller permissions.
-TODO: Change to a single list of [{}] where the internal {} is as userCommands etc currently are, and its index in the outer list corresponds to command access level
-"""
 class HeirarchicalCommandsDB:
-    userCommands = {}
-    adminCommands = {}
-    devCommands = {}
-
+    """Class that stores, categorises, and calls commands based on a text name and caller permissions.
+    TODO: Change to a single list of [{}] where the internal {} is as userCommands etc currently are, and its index in the outer list corresponds to command access level
+    
+    :var userCommands: Dictionary of commands to tuples containing a function reference to call on call of that command, a boolean representing whether args should be converted to lower case before passing, and a boolean representing whether the function name is case-sensitive
+    :vartype userCommands: dict[str, tuple[builtin_function_or_method, bool, bool]]
+    :var adminCommands: Dictionary of commands to tuples containing a function reference to call on call of that command, a boolean representing whether args should be converted to lower case before passing, and a boolean representing whether the function name is case-sensitive
+    :vartype adminCommands: dict[str, tuple[builtin_function_or_method, bool, bool]]
+    :var devCommands: Dictionary of commands to tuples containing a function reference to call on call of that command, a boolean representing whether args should be converted to lower case before passing, and a boolean representing whether the function name is case-sensitive
+    :vartype devCommands: dict[str, tuple[builtin_function_or_method, bool, bool]]
+    """
 
     def __init__(self):
         self.userCommands = {}
         self.adminCommands = {}
         self.devCommands = {}
 
-
-    """
-    Register a command in the database.
-
-    @param command -- the text name users should call the function by. Commands are case sensitive.
-    @param function -- reference to the function that should be called
-    @param isAdmin -- whether the caller requires admin privilages to call the command. Default: False
-    @param isDev -- whether the caller requires developer privilages to call the command. Default: False  
-    """
+    
     def register(self, command, function, isAdmin=False, isDev=False, forceKeepArgsCasing=False, forceKeepCommandCasing = False):
+        """Register a command in the database.
+
+        :param str command: the text name users should call the function by. Commands are case sensitive.
+        :param builtin_function_or_method function: reference to the function that should be called
+        :param bool isAdmin: whether the caller requires admin privilages to call the command. (Default False)
+        :param bool isDev: whether the caller requires developer privilages to call the command. (Default False)  
+        """
         if isDev:
             self.devCommands[command if forceKeepCommandCasing else command.lower()] = (function, forceKeepArgsCasing, forceKeepCommandCasing)
         elif isAdmin:
@@ -30,18 +31,19 @@ class HeirarchicalCommandsDB:
         else:
             self.userCommands[command if forceKeepCommandCasing else command.lower()] = (function, forceKeepArgsCasing, forceKeepCommandCasing)
 
-    """
-    Call a command or send an error message.
-
-    @param command -- the text name of the command to attempt to call. Commands are case sensitive
-    @param message -- the discord message calling the command. This is required for referencing the author and sending responses
-    @param args -- string containing arguments to pass to the command
-    @param isAdmin -- whether the calling user has admin privilages. Default: False
-    @param isDev -- whether the calling user has developer privilages. Default: False
-    @param isDM -- Whether the command was called from DMs or not
-    @return -- True if the command call was successful, False otherwise
-    """
+    
     async def call(self, command, message, args, isAdmin=False, isDev=False, isDM=False):
+        """Call a command or send an error message.
+
+        :param str command: the text name of the command to attempt to call. Commands are case sensitive
+        :param discord.message message: the discord message calling the command. This is required for referencing the author and sending responses
+        :param str args: string containing arguments to pass to the command
+        :param bool isAdmin: whether the calling user has admin privilages. (Default False)
+        :param bool isDev: whether the calling user has developer privilages. (Default False)
+        :param bool isDM: Whether the command was called from DMs or not
+        :return: True if the command call was successful, False otherwise
+        :rtype: bool
+        """
         # First search user commands
         if command in self.userCommands and self.userCommands[command][2]:
             await self.userCommands[command][0](message, args if self.userCommands[command][1] else args.lower(), isDM)
--- BB/bbDatabases/bbUserDB.py
+++ BB/bbDatabases/bbUserDB.py
@@ -3,45 +3,47 @@ from .. import bbUtil
 from ..logging import bbLogger
 import traceback
 
-"""
-A database of bbUser objects.
-
-"""
 class bbUserDB:
+    """A database of bbUser objects.
+    
+    :var users: Dictionary of users in the database, where values are the bbUser objects and keys are the ids of their respective bbUser
+    :vartype users: dict[int, bbUser]
+    """
+
     def __init__(self):
         # Store users as a dict of user.id: user
         self.users = {}
 
 
-    """
-    Check if a user is stored in the database with the given ID.
-
-    @param id -- integer discord ID for the bbUser to search for
-    @return -- True if id corresponds to a user in the database, false if no user is found with the id
-    """
     def userIDExists(self, id):
+        """Check if a user is stored in the database with the given ID.
+
+        :param int id: integer discord ID for the bbUser to search for
+        :return: True if id corresponds to a user in the database, false if no user is found with the id
+        :rtype: bool
+        """
         return id in self.users.keys()
 
 
-    """
-    Check if a given bbUser object is stored in the database.
-    Currently only checks if a user with the same ID is stored in the database, not if the objects are the same.
-
-    @param user -- a bbUser object to check for existence
-    @return -- True if a bbUser is found in the database with a matching ID, False otherwise
-    """
     def userObjExists(self, user):
+        """Check if a given bbUser object is stored in the database.
+        Currently only checks if a user with the same ID is stored in the database, not if the objects are the same.
+
+        :param bbUser user: a bbUser object to check for existence
+        :return: True if a bbUser is found in the database with a matching ID, False otherwise
+        :rtype: bool
+        """
         return self.userIDExists(user.id)
 
 
-    """
-    Internal function to assert the type of and, potentially cast, an ID.
-
-    @param ID -- the ID to type check. Can be either int or a string consisting only of digits.
-    @throws TypeError -- If the ID does not conform to the above requirements.
-    @return -- ID if ID is an int, or int(ID) if ID is a string of digits.
-    """
     def validateID(self, id):
+        """Internal function to assert the type of and, potentially cast, an ID.
+
+        :param int ID: the ID to type check. Can be either int or a string consisting only of digits.
+        :throws TypeError: If the ID does not conform to the above requirements.
+        :return: ID if ID is an int, or int(ID) if ID is a string of digits.
+        :rtype: int
+        """
         # If ID is a string, ensure it can be casted to an int before casting and returning.
         if type(id) == str:
             if not bbUtil.isInt(id):
@@ -54,13 +56,12 @@ class bbUserDB:
         return id
     
 
-    """
-    Reset the stats for the user with the specified ID.
-
-    @param ID -- The ID of the user to reset. Can be integer or a string of digits.
-    @throws KeyError -- If no user is found with the requested ID
-    """
     def reinitUser(self, id):
+        """Reset the stats for the user with the specified ID.
+
+        :param int ID: The ID of the user to reset. Can be integer or a string of digits.
+        :throws KeyError: If no user is found with the requested ID
+        """
         id = self.validateID(id)
         # ensure the ID exists in the database
         if not self.userIDExists(id):
@@ -69,14 +70,15 @@ class bbUserDB:
         self.users[id].resetUser()
 
 
-    """
-    Create a new bbUser object with the specified ID and add it to the database
-
-    @param id -- integer discord ID for the user to add
-    @throws KeyError -- If a bbUser already exists in the database with the specified ID
-    @return -- the newly created bbUser
-    """
     def addUser(self, id):
+        """
+        Create a new bbUser object with the specified ID and add it to the database
+
+        :param int id: integer discord ID for the user to add
+        :throws KeyError: If a bbUser already exists in the database with the specified ID
+        :return: the newly created bbUser
+        :rtype: bbUser
+        """
         id = self.validateID(id)
         # Ensure no user exists with the specified ID in the database
         if self.userIDExists(id):
@@ -86,13 +88,12 @@ class bbUserDB:
         self.users[id] = newUser
         return newUser
 
-    """
-    Store the given bbUser object in the database
-
-    @param userObj -- bbUser to store
-    @throws KeyError -- If a bbUser already exists in the database with the same ID as the given bbUser
-    """
     def addUserObj(self, userObj):
+        """Store the given bbUser object in the database
+
+        :param bbUser userObj: bbUser to store
+        :throws KeyError: If a bbUser already exists in the database with the same ID as the given bbUser
+        """
         # Ensure no bbUser exists in the db with the same ID as the given bbUser
         if self.userIDExists(userObj.id):
             raise KeyError("Attempted to add a user that is already in this bbUserDB: " + str(userObj))
@@ -100,65 +101,64 @@ class bbUserDB:
         self.users[userObj.id] = userObj
 
 
-    """
-    If a bbUser exists in the database with the requested ID, return it. If not, create and store a new bbUser and return it.
-
-    @param id -- integer discord ID for the user to fetch or create
-    @return -- the requested/created bbUser
-    """
     def getOrAddID(self, id):
+        """If a bbUser exists in the database with the requested ID, return it. If not, create and store a new bbUser and return it.
+
+        :param int id: integer discord ID for the user to fetch or create
+        :return: the requested/created bbUser
+        :rtype: int
+        """
         return self.getUser(id) if self.userIDExists(id) else self.addUser(id)
 
     
-    """
-    Remove the new bbUser object with the specified ID from the database
-    ⚠ The bbUser object is deleted from memory.
-
-    @param id -- integer discord ID for the user to remove
-    @throws KeyError -- If no bbUser exists in the database with the specified ID
-    """
     def removeUser(self, id):
+        """Remove the new bbUser object with the specified ID from the database
+        ⚠ The bbUser object is deleted from memory.
+
+        :param int id: integer discord ID for the user to remove
+        :throws KeyError: If no bbUser exists in the database with the specified ID
+        """
         id = self.validateID(id)
         if not self.userIDExists(id):
             raise KeyError("user not found: " + str(id))
         del self.users[id]
 
-
-    """
-    Fetch the bbUser from the database with the given ID.
-
-    @param ID -- integer discord ID for the user to fetch
-    @return -- the stored bbUser with the given ID
-    """
+    
     def getUser(self, id):
+        """Fetch the bbUser from the database with the given ID.
+
+        :param int ID: integer discord ID for the user to fetch
+        :return: the stored bbUser with the given ID
+        :rtype: bbUser
+        """
         id = self.validateID(id)
         return self.users[id]
 
 
-    """
-    Get a list of all bbUser objects stored in the database
-
-    @return -- list containing all bbUser objects in the db
-    """
     def getUsers(self):
+        """Get a list of all bbUser objects stored in the database
+
+        :return: list containing all bbUser objects in the db
+        :rtype: list[bbUser]
+        """
         return list(self.users.values())
 
     
-    """
-    Get a list of all user IDs stored in the database
-
-    @return -- list containing all int discord IDs for which bbUsers are stored in the database
-    """
     def getIds(self):
+        """Get a list of all user IDs stored in the database
+
+        :return: list containing all int discord IDs for which bbUsers are stored in the database
+        :rtype: list[int]
+        """
         return list(self.users.keys())
 
     
-    """
-    Serialise this bbUserDB into dictionary format.
-
-    @return -- A dictionary containing all data needed to recreate this bbUserDB
-    """
     def toDict(self):
+        """Serialise this bbUserDB into dictionary format.
+
+        :return: A dictionary containing all data needed to recreate this bbUserDB
+        :rtype: dict
+        """
         data = {}
         # Iterate over all user IDs in the database
         for id in self.getIds():
@@ -170,24 +170,24 @@ class bbUserDB:
                 bbLogger.log("UserDB", "toDict", "Error serialising bbUser: " + e.__class__.__name__, trace=traceback.format_exc(), eventType="USERERR")
         return data
 
-
-    """
-    Get summarising information about this bbUserDB in string format.
-    Currently only the number of users stored.
-
-    @return -- A string containing summarising info about this db
-    """
+    
     def __str__(self):
+        """Get summarising information about this bbUserDB in string format.
+        Currently only the number of users stored.
+
+        :return: A string containing summarising info about this db
+        :rtype: str
+        """
         return "<bbUserDB: " + str(len(self.users)) + " users>"
 
 
-"""
-Construct a bbUserDB from a dictionary-serialised representation - the reverse of bbUserDB.toDict()
-
-@param userDBDict -- a dictionary-serialised representation of the bbUserDB to construct
-@return -- the new bbUserDB
-"""
 def fromDict(userDBDict):
+    """Construct a bbUserDB from a dictionary-serialised representation - the reverse of bbUserDB.toDict()
+
+    :param dict userDBDict: a dictionary-serialised representation of the bbUserDB to construct
+    :return: the new bbUserDB
+    :rtype: bbUserDB
+    """
     # Instance the new bbUserDB
     newDB = bbUserDB()
     # iterate over all user IDs to spawn
