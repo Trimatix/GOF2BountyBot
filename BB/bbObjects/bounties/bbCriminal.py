5f0420928be12c3b98196cd22ff0e9d3a931b99b
Trimatix <1jasperlaw@gmail.com>
2020-09-15 23:59:21 +0100

    add docstrings and type hints to bbCriminal

--- BB/bbObjects/bounties/bbCriminal.py
+++ BB/bbObjects/bounties/bbCriminal.py
@@ -3,18 +3,40 @@ from .. import bbAliasable
 from ..items import bbShip
 
 class Criminal (bbAliasable.Aliasable):
-    name = ""
-    faction = ""
-    icon = ""
-    wiki = ""
-    hasWiki = False
-    isPlayer = False
-    builtIn = False
+    """
+    A criminal to be wanted in bounties.
 
-    ship = None
-    hasShip = False
+    :var name: The name of the criminal
+    :vartype name: str
+    :var faction: the faction that this criminal is wanted by
+    :vartype faction: str
+    :var icon: A URL pointing to an image to use as the criminal's icon
+    :vartype icon: str
+    :var wiki: A URL pointing to a web page to use as this criminal's wiki page in their info embed
+    :vartype wiki: str
+    :var hasWiki: Whether or not this criminal has a wiki page
+    :vartype hasWiki: bool
+    :var isPlayer: Whether this criminal is a player or an NPC
+    :vartype isPlayer: bool
+    :var builtIn: If this criminal is an NPC, are they built in or custom?
+    :vartype builtIn: bool
+    :var ship: The ship equipped by this criminal
+    :vartype ship: bbShip
+    :var hasShip: Whether this criminal has a ship equipped or not
+    :vartype hasShip: bool
+    """
 
-    def __init__(self, name, faction, icon, builtIn=False, isPlayer=False, aliases=[], wiki="", ship=None):
+    def __init__(self, name : str, faction : str, icon, builtIn=False, isPlayer=False, aliases=[], wiki="", ship=None):
+        """
+        :param str name: The name of the criminal
+        :param str faction: the faction that this criminal is wanted by
+        :param str icon: A URL pointing to an image to use as the criminal's icon
+        :param str wiki: A URL pointing to a web page to use as this criminal's wiki page in their info embed
+        :param bool isPlayer: Whether this criminal is a player or an NPC
+        :param bool builtIn: If this criminal is an NPC, are they built in or custom?
+        :param bbShip ship: The ship equipped by this criminal
+        :param list[str] aliases: Alias names that can be used to refer to this criminal
+        """
         super(Criminal, self).__init__(name, aliases)
         if name == "":
             raise RuntimeError("CRIM_CONS_NONAM: Attempted to create a Criminal with an empty name")
@@ -39,6 +61,10 @@ class Criminal (bbAliasable.Aliasable):
 
     
     def clearShip(self):
+        """Delete the equipped ship, removing it from memory
+
+        :raise RuntimeError: If the criminal does not have a ship equipped
+        """
         if not self.hasShip:
             raise RuntimeError("CRIM_CLEARSH_NOSHIP: Attempted to clearShip on a Criminal with no active ship")
         del self.ship
@@ -46,42 +72,78 @@ class Criminal (bbAliasable.Aliasable):
 
 
     def unequipShip(self):
+        """unequip the equipped ship, without deleting the object
+
+        :raise RuntimeError: If the criminal does not have a ship equipped
+        """
         if not self.hasShip:
             raise RuntimeError("CRIM_UNEQSH_NOSHIP: Attempted to unequipShip on a Criminal with no active ship")
         self.ship = None
         self.hasShip = False
 
 
-    def equipShip(self, ship):
+    def equipShip(self, ship : bbShip):
+        """Equip the given ship, by reference to the given object
+
+        :param bbShip ship: The ship to equip
+        :raise RuntimeError: If the criminal already has a ship equipped
+        """
         if self.hasShip:
             raise RuntimeError("CRIM_EQUIPSH_HASSH: Attempted to equipShip on a Criminal that already has an active ship")
         self.ship = ship
         self.hasShip = True
 
     
-    def copyShip(self, ship):
+    def copyShip(self, ship : bbShip):
+        """Equip the given ship, by taking a deep copy of the given object
+
+        :param bbShip ship: The ship to equip
+        :raise RuntimeError: If the criminal already has a ship equipped
+        """
         if self.hasShip:
             raise RuntimeError("CRIM_COPYSH_HASSH: Attempted to copyShip on a Criminal that already has an active ship")
         self.ship = bbShip.fromDict(ship.toDict())
         self.hasShip = True
         
 
-    def getType(self):
+    def getType(self) -> type:
+        """⚠ DEPRACATED
+        Get the type of this object
+
+        :return: The Criminal class
+        :rtype: type
+        """
         return Criminal
 
 
-    def __hash__(self):
+    def __hash__(self) -> int:
+        """Provide a hash of this object based on the object's location in memory.
+
+        :return: A hash of this criminal
+        :rtype: int
+        """
         return hash(repr(self))
 
 
-    def toDict(self):
+    def toDict(self) -> dict:
+        """Serialize this criminal into dictionary format, for saving to file.
+        
+        :return: A dictionary containing all data necessary to replicate this object
+        :rtype: dict
+        """
         if self.builtIn:
             return {"builtIn":True, "name":self.name}
         else:
             return {"builtIn":False, "isPlayer": self.isPlayer, "name":self.name, "icon":self.icon, "faction":self.faction, "aliases":self.aliases, "wiki":self.wiki}
 
 
-def fromDict(crimDict, builtIn=False):
+def fromDict(crimDict : dict, builtIn=False) -> bbCriminal:
+    """Factory function that will either provide a reference to a builtIn bbCriminal if a builtIn criminal is requested, or construct a new bbCriminal object from the provided data.
+
+    :param dict crimDict: A dictionary containing all data necessary to construct the desired bbCriminal. If the criminal is builtIn, this need only be their name, "builtIn": True, and possibly the equipped ship.
+    :return: The requested bbCriminal object reference
+    :rtype: bbCriminal
+    """
     if "builtIn" in crimDict:
         if crimDict["builtIn"]:
             return bbData.builtInCriminalObjs[crimDict["name"]]
