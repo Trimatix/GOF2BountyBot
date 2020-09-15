b89e63b40b5b96acf522dc39f19f5ef861b66a3b
Trimatix <1jasperlaw@gmail.com>
2020-09-15 22:34:03 +0100

    add docstrings and type hints to bbBounty

--- BB/bbObjects/bounties/bbBounty.py
+++ BB/bbObjects/bounties/bbBounty.py
@@ -1,18 +1,40 @@
 from . import bbBountyConfig
 from ...bbConfig import bbData
 from . import bbCriminal
+from typing import Dict, Union
 
 class Bounty:
-    criminal = None
-    issueTime = -1
-    route = []
-    reward = 0.0
-    endTime = -1
-    faction = ""
-    checked = {}
-    answer = ""
+    """A bounty listing for a criminal, to be hunted down by players.
+
+    :var criminal: The criminal who is being hunted
+    :vartype criminal: bbCriminal
+    :var issueTime: The time at which the bounty was created
+    :vartype issueTime: datetime.datetime
+    :var route: the names of systems that are in the route
+    :vartype route: list[str]
+    :var reward: the number of credits available to contributing players
+    :vartype reward: int
+    :var endTime: the time at which the bounty should automatically expire
+    :vartype endTime: datetime.datetime
+    :var faction: the faction to which this bounty belongs
+    :vartype faction: str
+    :var checked: A dictionary tracking which player checked each system. Keys are system names, values are user ids. values for unchecked systems are -1.
+    :vartype checked: dict[str, int]
+    :var answer: The name of the system where the criminal is located
+    :vartype answer: str
+    """
 
     def __init__(self, criminalObj=None, config=None, bountyDB=None, dbReload=False):
+        """
+        :param criminalObj: The criminal to be wanted. Give None to randomly generate a criminal. (Default None)
+        :type criminalObj: bbCriminal or None
+        :param config: a bountyconfig describing all aspects of this bounty. Give None to randomly generate one. (Default None)
+        :type config: bbBountyConfig or None
+        :param bountyDB: The database of currenly active bounties. This is required unless dbReload is True. (Default None)
+        :type bountyDB: bbBountyDB or None
+        :param bool dbReload: Give True if this bounty is being created during bot bootup, False otherwise. This currently toggles whether the passed bounty is checked for existence or not. (Default False)
+        :raise ValueError: When dbReload is False but bountyDB is not given
+        """
         if not dbReload and bountyDB is None:
             raise ValueError("Bounty constructor: No bounty database given")
         makeFresh = criminalObj is None
@@ -51,7 +73,14 @@ class Bounty:
     # return 1 => system already checked
     # return 2 => system was unchecked, but is not the answer
     # return 3 => win!
-    def check(self, system, userID):
+    def check(self, system : str, userID : int) -> int:
+        """Check a system along the route.
+
+        :param str system: The name of the system to check
+        :param int userID: The id of the user checking the system
+        :return: A symbollic integer representing the result of the check. 0 => This system is not in the bounty route. 1 => this system has already been checked. 2 => The system was unchecked, but is not the answer. 3 => answer found.
+        :rtype: int
+        """
         if system not in self.route:
             return 0
         elif self.systemChecked(system):
@@ -62,10 +91,21 @@ class Bounty:
                 return 3
             return 2
 
-    def systemChecked(self, system):
+    def systemChecked(self, system : str) -> bool:
+        """Decide whether or not a system has been checked.
+        
+        :param str system: The system to inspect for checking
+        :return: True if system has been checked yet, False otherwise
+        :rtype: bool
+        """
         return self.checked[system] != -1
 
-    def calcRewards(self):
+    def calcRewards(self) -> Dict[int, Dict[str, Union[int, bool]]]:
+        """Calculate the winning user, and how many credits (and, in the future, xp points) to award to which contributing users.
+
+        :return: A dictionary of user IDs to rewards. rewards are given as a dict, giving the number of systems checked, the reward credits, and whether this user ID won or not.
+        :rtype: dict[int, dict[str, int or bool]]]
+        """
         rewards = {}
         checkedSystems = 0
         for system in self.route:
@@ -86,12 +126,21 @@ class Bounty:
                     rewards[self.checked[system]]["reward"] += int(self.reward / len(self.route))
         return rewards
 
-    def toDict(self):
+    def toDict(self) -> dict:
+        """Serialize this bounty to dictionary, to be saved to file.
+
+        :return: A dictionary representation of this bounty.
+        :rtype: dict
+        """
         return {"faction": self.faction, "route": self.route, "answer": self.answer, "checked": self.checked, "reward": self.reward, "issueTime": self.issueTime, "endTime": self.endTime, "criminal": self.criminal.toDict()}
 
 
+def fromDict(bounty : dict, dbReload=False) -> bbBounty:
+    """Factory function constructing a new bbBounty from a dictionary serialized description - the opposite of bbBounty.toDict
 
-def fromDict(bounty, dbReload=False):
+    :param dict bounty: Dictionary containing all information needed to construct the desired bbBounty
+    :param bool dbReload: Give True if this bounty is being created during bot bootup, False otherwise. This currently toggles whether the passed bounty is checked for existence or not. (Default False)
+    """
     return Bounty(dbReload=dbReload,
                     criminalObj=bbCriminal.fromDict(bounty["criminal"]), 
                     config=bbBountyConfig.BountyConfig(faction=bounty["faction"], route=bounty["route"], answer=bounty["answer"], checked=bounty["checked"], reward=bounty["reward"], issueTime=bounty["issueTime"], endTime=bounty["endTime"]))
