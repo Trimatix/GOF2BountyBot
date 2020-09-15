9d36ff0b2199576909ecce8b82e82ad9425087c8
Trimatix <1jasperlaw@gmail.com>
2020-09-15 23:44:26 +0100

    add docstrings and type hints to bbBountyConfig

--- BB/bbObjects/bounties/bbBountyConfig.py
+++ BB/bbObjects/bounties/bbBountyConfig.py
@@ -3,25 +3,82 @@ from datetime import datetime, timedelta
 
 from ...bbConfig import bbData, bbConfig
 from ... import bbUtil
+from ...bbDatabases import bbBountyDB
 
 class BountyConfig:
-    faction = ""
-    name = ""
-    isPlayer = ""
-    route = []
-    start = ""
-    end = ""
-    answer = ""
-    checked = {}
-    reward = -1.0
-    issueTime = -1.0
-    endTime = -1.0
-    icon = ""
-    builtIn = False
-    generated = False
-    ship = None
+    """
+    Configurator class describing all attributes needed for a bbBounty object.
+
+    :var faction: The faction owning this bounty
+    :vartype faction: str
+    :var name: The name of the wanted criminal. If this is a player bounty, name should be the player mention.
+    :vartype name: str
+    :var isPlayer: Whether or not the target criminal is a player or an npc
+    :vartype isPlayer: bool
+    :var route: the names of systems in this bounty's route
+    :vartype route: list[str]
+    :var start: The name of the system at the start of the route
+    :vartype start: str
+    :var end: The name of the system at the end of the route
+    :vartype end: str
+    :var answer: The name of the system where the criminal is located
+    :vartype answer: str
+    :var checked: Dictionary of system names to user IDs, where the id corresponds to the user who checked that system, or -1 if the system is unchecked.
+    :vartype checked: dict[str, int]
+    :var reward: Prize pool of credits to award to contributing users
+    :vartype reward: int
+    :var issueTime: A utc timestamp representing the time at which the bounty was issued
+    :vartype issueTime: float
+    :var endTime: A utc timestamp representing the time at which the bounty should automatically expire
+    :vartype endTime: flaot
+    :var icon: A URL directly linking to an image to use as the criminal's icon
+    :vartype icon: str
+    :var aliases: Aliases that can be used to refer to this criminal
+    :vartype aliases: list[str]
+    :var wiki: The page to link to as the criminal's wiki, in their info embed
+    :vartype wiki: str
+    :var builtIn: whether or not this is a built in npc criminal
+    :vartype builtIn: bool
+    :var generated: whether or not this config is ready to be used. The config must verify and generate its attributes before they can be used in a bbBounty.
+    :vartype generated: bool
+    :var ship: The bbShip this criminal should equip
+    :vartype ship: bbShip
+    """
 
     def __init__(self, faction="", name="", isPlayer=None, route=[], start="", end="", answer="", checked={}, reward=-1, issueTime=-1.0, endTime=-1.0, icon="", aliases=[], wiki="", ship=None):
+        """All parameters are optional. If a parameter is not given, it will be randomly generated.
+
+        :param faction: The faction owning this bounty
+        :type faction: str
+        :param name: The name of the wanted criminal. If this is a player bounty, name should be the player mention.
+        :type name: str
+        :param isPlayer: Whether or not the target criminal is a player or an npc
+        :type isPlayer: bool
+        :param route: the names of systems in this bounty's route
+        :type route: list[str]
+        :param start: The name of the system at the start of the route
+        :type start: str
+        :param end: The name of the system at the end of the route
+        :type end: str
+        :param answer: The name of the system where the criminal is located
+        :type answer: str
+        :param checked: Dictionary of system names to user IDs, where the id corresponds to the user who checked that system, or -1 if the system is unchecked.
+        :type checked: dict[str, int]
+        :param reward: Prize pool of credits to award to contributing users
+        :type reward: int
+        :param issueTime: A utc timestamp representing the time at which the bounty was issued
+        :type issueTime: float
+        :param endTime: A utc timestamp representing the time at which the bounty should automatically expire
+        :type endTime: flaot
+        :param icon: A URL directly linking to an image to use as the criminal's icon
+        :type icon: str
+        :param aliases: Aliases that can be used to refer to this criminal
+        :type aliases: list[str]
+        :param wiki: The page to link to as the criminal's wiki, in their info embed
+        :type wiki: str
+        :param ship: The bbShip this criminal should equip
+        :type ship: bbShip
+        """
         self.faction = faction.lower()
         self.name = name.title()
         self.isPlayer = False if isPlayer is None else isPlayer
@@ -48,7 +105,17 @@ class BountyConfig:
         self.ship = ship
         
     
-    def generate(self, bountyDB, noCriminal=True, forceKeepChecked=False, forceNoDBCheck=False):
+    def generate(self, bountyDB : bbBountyDB.bbBountyDB, noCriminal=True, forceKeepChecked=False, forceNoDBCheck=False):
+        """Validate all given config data, and randomly generate missing data.
+
+        :param bbBountyDB bountyDB: Database containing all currently active bounties. When forceNoDBCheck is True, this is ignored.
+        :param bool noCriminal: If this is True, randomly generate a bbCriminal object. (Default True)
+        :param bool forceKeepChecked: If this is False, a blank checked dictionary will be used. This should only be set to be True when using a pre-made checked dictionary; e.g for custom bounties or for bounties loaded from file. (Default False)
+        :param bool forceNoDBCheck: If this is False, do not check if the bounty already exists. This should only be used as a performance and compatibility measure when loading in a bounty from file. (Default False)
+        :raise ValueError: When requesting an invalid faction, or when requesting an invalid reward amount
+        :raise IndexError: When no space is available for a new bounty
+        :raise KeyError: When the requested criminal name already exists in a bounty, or when requesting an unknown system name
+        """
         doDBCheck = not forceNoDBCheck
         if noCriminal:
             if self.name in bbData.bountyNames:
