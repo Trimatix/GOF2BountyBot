ee78749afa53c8aef13f3df4c2aff5bbf6635125
Trimatix <1jasperlaw@gmail.com>
2020-09-16 19:36:36 +0100

    add docstrings and type hints to bbAliasable

--- BB/bbObjects/bbAliasable.py
+++ BB/bbObjects/bbAliasable.py
@@ -1,8 +1,24 @@
 from abc import ABC, abstractmethod
-
+from typing import List
 
 class Aliasable (ABC):
-    def __init__(self, name, aliases, forceAllowEmpty=False):
+    """An abstract class allowing subtype instances to be identified and compared by any list of names (aliases).
+    A great example and common use case is in BountyBot's bbCriminal class. Criminals are NPCs that each have a unique name.
+    These names usually consist of a forename and sirname, for example 'Ganfor Kant'. Providing 'Ganfor' and 'Kant' as aliases
+    allows the Ganfor Kant object to be identified by any of 'Ganfor', 'Kant', or 'Ganfor Kant', for user convenience.
+
+    :var name: The main identifier for the object
+    :vartype name: str
+    :var aliases: A list of alternative identifiers for the object
+    :vartype aliases: list[str]
+    """
+
+    def __init__(self, name : str, aliases : List[str], forceAllowEmpty=False):
+        """
+        :param str name: The main identifier for the object
+        :param list[str] aliases: A list of alternative identifiers for the object
+        :param bool forceAllowEmpty: By default, "" is disallowed as an alias. Give True to force allow it (Default False)
+        """
         if not name and not forceAllowEmpty:
             raise RuntimeError("ALIAS_CONS_NONAM: Attempted to create an aliasable with an empty name")
         self.name = name
@@ -16,20 +32,47 @@ class Aliasable (ABC):
         if name.lower() not in aliases:
             self.aliases += [name.lower()]
     
-    def __eq__(self, other):
+
+    def __eq__(self, other : Aliasable) -> bool:
+        """Decide Aliasable equality based on either object sharing the other's main name.
+        If neither object has the other's main name as an alias, False will be returned regardless of whether any other aliases are shared.
+
+        :param
+        """
         return type(other) == self.getType() and self.isCalled(other.name) or other.isCalled(self.name)
 
-    def isCalled(self, name):
+
+    def isCalled(self, name : str) -> bool:
+        """Decide whether the provided name is one of this object's aliases.
+
+        :param str name: The name to look up in this object's aliases
+        :return: True if name is either this object's name, or is one of this object's aliases.
+        :rtype: bool
+        """
         return name.lower() == self.name.lower() or name.lower() in self.aliases
 
-    def removeAlias(self, name):
+
+    def removeAlias(self, name : str):
+        """Remove the given name from this object's aliases. This does not affect the object's main name.
+
+        :param str name: The alias to remove
+        """
         if name.lower() in self.aliases:
             self.aliases.remove(name.lower())
 
-    def addAlias(self, name):
+
+    def addAlias(self, name : str):
+        """Add the given name to this object's aliases.
+
+        :param str name: The alias to add
+        """
         if name.lower() not in self.aliases:
             self.aliases.append(name.lower())
 
+
     @abstractmethod
     def getType(self):
+        """⚠ DEPRACATED
+        Get the type of this object. Must be overloaded by Aliasable subclasses.
+        """
         pass
