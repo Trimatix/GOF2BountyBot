09838d9760fc933b9847a25e1b564b36233be1a2
Trimatix <1jasperlaw@gmail.com>
2020-09-21 23:26:31 +0100

    add partial docstrings to ReactionMenu.py

--- BB/reactionMenus/ReactionMenu.py
+++ BB/reactionMenus/ReactionMenu.py
@@ -65,7 +65,7 @@ class ReactionMenuOption:
     :vartype addFunc: function
     :var removeFunc: The function to call when this option is removed by a user
     :vartype removeFunc: function
-    :var addArgs: The arguments to pass to addFunc. No type checking is done on this parameter, but a dict is recommended as a close alternative to keyword args.
+    :var addArgs: The arguments to pass to addFunc. No type checking is done on this parameter, but a dict is recommended as a close replacement for keyword args.
     :var removeArgs: The arguments to pass to removeFunc.
     :var addIsCoroutine: Whether or not addFuc is a coroutine and must be awaited
     :vartype addIsCoroutine: bool
@@ -87,7 +87,7 @@ class ReactionMenuOption:
         :param bbUtil.dumbEmoji emoji: The emoji that a user must react with to trigger this option
         :param function addFunc: The function to call when this option is added by a user
         :param function removeFunc: The function to call when this option is removed by a user
-        :param addArgs: The arguments to pass to addFunc. No type checking is done on this parameter, but a dict is recommended as a close alternative to keyword args.
+        :param addArgs: The arguments to pass to addFunc. No type checking is done on this parameter, but a dict is recommended as a close replacement for keyword args.
         :param removeArgs: The arguments to pass to removeFunc.
         """
         
@@ -108,9 +108,12 @@ class ReactionMenuOption:
     
 
     async def add(self, member : Member):
-        """
+        """Invoke this option's 'reaction added' functionality.
         This method is called by the owning reaction menu whenever this option is added by any user
         that matches the menu's restrictions, if any apply (e.g targetMember, targetRole)
+
+        :param discord.Member member: The member adding the reaction
+        :return: The result of the option's addFunc function, if one exists.
         """
         if self.addFunc is not None:
             if self.addIncludeUser:
@@ -123,6 +126,13 @@ class ReactionMenuOption:
 
 
     async def remove(self, member : Member):
+        """Invoke this option's 'reaction removed' functionality.
+        This method is called by the owning reaction menu whenever this option is removed by any user
+        that matches the menu's restrictions, if any apply (e.g targetMember, targetRole)
+
+        :param discord.Member member: The member that removed the reaction
+        :return: The result of the option's removeFunc function, if one exists.
+        """
         if self.removeFunc is not None:
             if self.removeIncludeUser:
                 if self.removeHasArgs:
@@ -134,35 +144,154 @@ class ReactionMenuOption:
 
 
     def __hash__(self) -> int:
+        """Calculate a hash of this menu option from its repr string.
+        As of writing, this is based on the object's memory location.
+
+        :return: A hash of this menu option
+        :rtype: int
+        """
         return hash(repr(self)) 
 
 
     @abstractmethod
     def toDict(self) -> dict:
+        """Serialize this menu option into dictionary format for saving to file.
+        This is a base, abstract definition that does not encode option functionality (i.e function calls and arguments).
+
+        A generic but rather messy implementation for saving an option's addFunc and removeFunc could be written as follows:
+        Saving:
+            - Get the name of the module where the function reference is located using func.__module__
+            - Get the name of the function reference using func.__name__
+        Where func is addFunc or removeFunc as needed.
+        
+        Loading:
+        Find the function reference from the module_name and function_name with one of:
+            - getattr(globals()[module_name], function_name)
+            - getattr(sys.modules[module_name], function_name)
+
+        This is obviously a less than ideal implementation, and there are likely to be other solutions.
+
+        TODO: Add type, similar to reaction menu todict, to allow dummy options to be recreated from dict
+        :return: A dictionary containing rudimentary information about the menu option, to be used in conjunction with other type-specific information when reconstructing this menu option.
+        :rtype: dict
+        """
         return {"name":self.name, "emoji": self.emoji.toDict()}
 
 
 class NonSaveableReactionMenuOption(ReactionMenuOption):
+    """A basic concrete class for instancing ReactionMenuOptions without the possibility of saving them to file.
+    When creating a ReactionMenuOption subclass that can be saved to file, do not inherit from this class.
+    Instead, inherit directly from ReactionMenuOption or another suitable subclass that is not marked as unsaveable.
+    """
+
     def __init__(self, name : str, emoji : bbUtil.dumbEmoji, addFunc=None, addArgs=None, removeFunc=None, removeArgs=None):
+        """
+        :param str name: The name of this option, as displayed in the menu embed.
+        :param bbUtil.dumbEmoji emoji: The emoji that a user must react with to trigger this option
+        :param function addFunc: The function to call when this option is added by a user
+        :param function removeFunc: The function to call when this option is removed by a user
+        :param addArgs: The arguments to pass to addFunc. No type checking is done on this parameter, but a dict is recommended as a close replacement for keyword args.
+        :param removeArgs: The arguments to pass to removeFunc.
+        """
         super(NonSaveableReactionMenuOption, self).__init__(name, emoji, addFunc=addFunc, addArgs=addArgs, removeFunc=removeFunc, removeArgs=removeArgs)
 
 
     def toDict(self) -> dict:
-        return super(NonSaveableReactionMenuOption, self).toDict()
+        """Unimplemented.
+        This class should only be used for reaction menu options that will not be saved to file.
+
+        :raise NotImplementedError: Always.
+        """
+        raise NotImplementedError("Attempted to call toDict on a non-saveable reaction menu option")
 
         
 class DummyReactionMenuOption(ReactionMenuOption):
+    """A reaction menu option with no function calls.
+    A prime example is ReactionPollMenu, where adding and removing options need not have any functionality.
+    """
     def __init__(self, name : str, emoji : bbUtil.dumbEmoji):
+        """
+        :param str name: The name of this option, as displayed in the menu embed.
+        :param bbUtil.dumbEmoji emoji: The emoji that a user must react with to trigger this option
+        """
         super(DummyReactionMenuOption, self).__init__(name, emoji)
 
 
     def toDict(self) -> dict:
+        """Serialize this menu option into dictionary format for saving to file.
+        Since dummy reaction menu options have no on-toggle functionality, the resulting base dictionary contains all information needed to 
+        reconstruct this option instance.
+
+        :return: A dictionary containing all necessary information to reconstruct this option instance
+        :rtype: dict
+        """
         return super(DummyReactionMenuOption, self).toDict()
 
 
 class ReactionMenu:
+    """A versatile class implementing emoji reaction menus.
+    How to use this class:
+    1. Send a message
+    2. Create a TimedTask that will call a menu deleting function (e.g deleteReactionMenu) with your new message ID on expiry TODO: add a constructor flag and TimedTask config data class to ReactionMenu constructor for autoscheduling menu expiry on menu creation
+    3. Pass your new message and TimedTask to the ReactionMenu constructor, also specifying a dictionary of menu options
+    4. Call updateMessage on your new ReactionMenu instance
+    5. Use discord's client events of either on_reaction_add and on_reaction_remove or on_raw_reaction_add and on_raw_reaction_remove to call your new menu's reactionAdded and reactionRemoved methods (bountybot.py has this behaviour already) TODO: Make reactionAdded and reactionRemoved ignore emoji that are not options in the menu
+
+    The real power of this class can be harnessed by binding function calls to individual menu option reactions when creating your options dictionary.
+    A great example of this is in ReactionRolePicker, which actually has no extra behaviour added over ReactionMenu. It acts more as a ReactionMenu preset,
+    defining a new constructor which transforms a dictionary of emojis to roles into an options dictionary, where each option's addFunc is bound to a role granting
+    function, and its removeFunc is bount to a role removing function. The only extra behaviour ReactionRolePickerOption implements over ReactionMenuOption is
+    the addition of its associated role ID being saved during toDict.
+
+    :var msg: the message where this menu is embedded
+    :vartype msg: discord.Message
+    :var options: A dictionary storing all of the menu's options and their behaviour
+    :vartype options: dict[bbUtil.dumbEmoji, ReactionMenuOption]
+    :var titleTxt: The content of the embed title
+    :vartype titleTxt: str
+    :var desc: he content of the embed description; appears at the top below the title
+    :vartype desc: str
+    :var col: The colour of the embed's side strip
+    :vartype col: discord.Colour
+    :var footerTxt: Secondary description appearing in darker font at the bottom of the embed
+    :vartype footerTxt: str
+    :var img: URL to a large icon appearing as the content of the embed, left aligned like a field
+    :vartype img: str
+    :var thumb: URL to a larger image appearing to the right of the title
+    :vartype thumb: str
+    :var icon: URL to a smaller image to the left of authorName. AuthorName is required for this to be displayed.
+    :vartype icon: str
+    :var authorName: Secondary, smaller title for the embed
+    :vartype authorName: str
+    :var timeout: The TimedTask responsible for expiring this menu
+    :vartype timeout: TimedTask
+    :var targetMember: The only discord.Member that is able to interact with this menu. All other reactions are ignored
+    :vartype targetMember: discord.Member
+    :var targetRole: In order to interact with this menu, users must possess this role. All other reactions are ignored
+    :vartype targetRole: discord.Role
+    :var saveable: Class attribute indicating whether or not this type of ReactionMenu can be saved to file. If not, this menu will be forcibly deleted before bot shutdown.
+    :vartype saveable: bool
+    """
+
     def __init__(self, msg : Message, options={}, 
                     titleTxt="", desc="", col=None, timeout=None, footerTxt="", img="", thumb="", icon="", authorName="", targetMember=None, targetRole=None):
+        """
+        :param discord.Message msg: the message where this menu is embedded
+        :param options: A dictionary storing all of the menu's options and their behaviour
+        :type options: dict[bbUtil.dumbEmoji, ReactionMenuOption]
+        :param str titleTxt: The content of the embed title
+        :param str desc: he content of the embed description; appears at the top below the title
+        :param discord.Colour col: The colour of the embed's side strip
+        :param str footerTxt: Secondary description appearing in darker font at the bottom of the embed
+        :param str img: URL to a large icon appearing as the content of the embed, left aligned like a field
+        :param str thumb: URL to a larger image appearing to the right of the title
+        :param str icon: URL to a smaller image to the left of authorName. AuthorName is required for this to be displayed.
+        :param str authorName: Secondary, smaller title for the embed
+        :param TimedTask timeout: The TimedTask responsible for expiring this menu
+        :param discord.Member targetMember: The only discord.Member that is able to interact with this menu. All other reactions are ignored
+        :param discord.Role targetRole: In order to interact with this menu, users must possess this role. All other reactions are ignored
+        """
+
         if footerTxt == "" and timeout is not None:
             footerTxt = "This menu will expire in " + bbUtil.td_format_noYM(timeout.expiryDelta) + "."
         
@@ -186,6 +315,12 @@ class ReactionMenu:
 
     
     def hasEmojiRegistered(self, emoji : bbUtil.dumbEmoji) -> bool:
+        """Decide whether or not the given emoji is an option in this menu
+
+        :param bbUtil.dumbEmoji emoji: The emoji to test for membership
+        :return: True if emoji is an option in this menu, False otherwise.
+        :rtype: bool
+        """
         return emoji in self.options
 
 
