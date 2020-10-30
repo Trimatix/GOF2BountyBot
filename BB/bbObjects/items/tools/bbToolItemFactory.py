bbffe708395515fbf4a986f1368c5a784235bca0
Trimatix <1jasperlaw@gmail.com>
2020-10-30 23:19:22 +0000

    implement bbToolItemFactory

---
+++ BB/bbObjects/items/tools/bbToolItemFactory.py
@@ -0,0 +1,16 @@
+from . import bbToolItem, bbShipSkinTool
+
+toolTypeConstructors = {"bbShipSkinTool": bbShipSkinTool.fromDict}
+
+def fromDict(toolDict : dict) -> bbToolItem.bbToolItem:
+    """Construct a bbToolItem from its dictionary-serialized representation.
+    This method decodes which tool constructor is appropriate based on the 'type' attribute of the given dictionary.
+
+    :param dict toolDict: A dictionary containing all information needed to construct the required bbToolItem. Critically, a name, type, and builtIn specifier.
+    :return: A new bbToolItem object as described in toolDict
+    :rtype: bbToolItem.bbToolItem
+    :raise NameError: When toolDict does not contain a 'type' attribute.
+    """
+    if "type" not in toolDict:
+        raise NameError("Required dictionary attribute missing: 'type'")
+    return toolTypeConstructors[toolDict["type"]](toolDict)
