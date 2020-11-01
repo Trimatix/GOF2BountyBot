232dcf447b645b6701d5f56b6dba72c694f90123
Trimatix <1jasperlaw@gmail.com>
2020-11-01 17:08:51 +0000

    temporarily disable admin_cmd_showmeHD. 240p renders already take ages on free tier AWS, let's not try 1080p.

--- BB/bountybot.py
+++ BB/bountybot.py
@@ -775,6 +775,16 @@ async def err_tempDisabled(message : discord.Message, args : str, isDM : bool):
     await message.channel.send(":x: All bounty/shop behaviour is currently disabled while I work on new features \:)")
 
 
+async def err_tempPerfDisabled(message : discord.Message, args : str, isDM : bool):
+    """Send an error message when a command is requested that is disabled for perfornance reasons.
+
+    :param discord.Message message: the discord message calling the command
+    :param str args: ignored
+    :param bool isDM: ignored
+    """
+    await message.channel.send(":x: This command has been temporarily disabled as it requires too much processing power. It may return in the future once hosting hardware has been upgraded! \:)")
+
+
 def randomColour():
     """Generate a completely random discord.Colour.
 
@@ -4433,8 +4443,10 @@ async def cmd_showmeHD(message : discord.Message, args : str, isDM : bool):
 
     await endLongProcess(waitMsg)
 
-bbCommands.register("showmehd", cmd_showmeHD, isAdmin=True)
-dmCommands.register("showmehd", cmd_showmeHD, isDev=True)
+# bbCommands.register("showmehd", cmd_showmeHD, isAdmin=True)
+# dmCommands.register("showmehd", cmd_showmeHD, isDev=True)
+bbCommands.register("showmehd", err_tempPerfDisabled, isAdmin=True)
+dmCommands.register("showmehd", err_tempPerfDisabled, isDev=True)
 
 
 
