bcc092cf7625cab150d6dc2ae8e710e6b06f9a05
Trimatix <1jasperlaw@gmail.com>
2020-11-03 19:51:52 +0000

    use IDs for homeguilds, rather than discord.Guild objects.

--- BB/bbObjects/bbUser.py
+++ BB/bbObjects/bbUser.py
@@ -74,9 +74,9 @@ class bbUser:
     :vartype dailyBountyWinsReset: datetime.datetime
     :var pollOwned: Whether or not this user has a running ReactionPollMenu
     :vartype pollOwned: bool
-    :var homeGuild: This user's 'home guild' - the only guild from which they may use several commands e.g buy and check.
-    :vartype homeGuild discord.Guild
-    :var guildTransferCooldownEnd: A timestamp after which this user is allowed to transfer their homeGuild.
+    :var homeGuildID: The id of this user's 'home guild' - the only guild from which they may use several commands e.g buy and check.
+    :vartype homeGuildID: int
+    :var guildTransferCooldownEnd: A timestamp after which this user is allowed to transfer their homeGuildID.
     :vartype guildTransferCooldownEnd: datetime.datetime
     """
 
@@ -85,7 +85,7 @@ class bbUser:
                     inactiveShips=bbInventory.bbInventory(), inactiveModules=bbInventory.bbInventory(), inactiveWeapons=bbInventory.bbInventory(), inactiveTurrets=bbInventory.bbInventory(),
                     lastSeenGuildId=-1, duelWins=0, duelLosses=0, duelCreditsWins=0, duelCreditsLosses=0,
                     alerts={}, bountyWinsToday=0, dailyBountyWinsReset=datetime.utcnow(), pollOwned=False,
-                    homeGuild=None, guildTransferCooldownEnd=datetime.utcnow()):
+                    homeGuildID=-1, guildTransferCooldownEnd=datetime.utcnow()):
         """
         :param int id: The user's unique ID. The same as their unique discord ID.
         :param int credits: The amount of credits (currency) this user has (Default 0)
@@ -108,8 +108,8 @@ class bbUser:
         :param int bountyWinsToday: The number of bounties the user has won today (Default 0)
         :param datetime.datetime dailyBountyWinsReset: A datetime.datetime representing the time at which the user's bountyWinsToday should be reset to zero (Default datetime.utcnow())
         :param bool pollOwned: Whether or not this user has a running ReactionPollMenu (Default False)
-        :param Guild homeGuild: This user's 'home guild' - the only guild from which they may use several commands e.g buy and check.
-        :param datetime.datetime guildTransferCooldownEnd: A timestamp after which this user is allowed to transfer their homeGuild.
+        :param Guild homeGuildID: The ID of this user's 'home guild' - the only guild from which they may use several commands e.g buy and check.
+        :param datetime.datetime guildTransferCooldownEnd: A timestamp after which this user is allowed to transfer their homeGuildID.
         :raise TypeError: When given an argument of incorrect type
         """
         if type(id) == float:
@@ -194,7 +194,7 @@ class bbUser:
 
         self.pollOwned = pollOwned
 
-        self.homeGuild = homeGuild
+        self.homeGuildID = homeGuildID
         self.guildTransferCooldownEnd = guildTransferCooldownEnd
 
     
@@ -216,7 +216,7 @@ class bbUser:
         self.duelCreditsWins = 0
         self.duelCreditsLosses = 0
         self.pollOwned = False
-        self.homeGuild = None
+        self.homeGuildID = -1
         self.guildTransferCooldownEnd = datetime.utcnow()
 
 
@@ -398,7 +398,7 @@ class bbUser:
                 "inactiveModules":inactiveModulesDict, "inactiveWeapons":inactiveWeaponsDict, "inactiveTurrets": inactiveTurretsDict, "lastSeenGuildId":self.lastSeenGuildId,
                 "duelWins": self.duelWins, "duelLosses": self.duelLosses, "duelCreditsWins": self.duelCreditsWins, "duelCreditsLosses": self.duelCreditsLosses,
                 "bountyWinsToday": self.bountyWinsToday, "dailyBountyWinsReset": self.dailyBountyWinsReset.timestamp(), "pollOwned": self.pollOwned,
-                "homeGuild": -1 if self.homeGuild is None else self.homeGuild.id, "guildTransferCooldownEnd": self.guildTransferCooldownEnd}
+                "homeGuildID": self.homeGuildID.id, "guildTransferCooldownEnd": self.guildTransferCooldownEnd}
 
 
     def userDump(self) -> str:
@@ -592,31 +592,31 @@ class bbUser:
         return self.isAlertedForType(UserAlerts.userAlertsIDsTypes[alertID], dcGuild, bbGuild, dcMember)
 
 
-    def hasHomeGuild(self) -> bool:
+    def hashomeGuild(self) -> bool:
         """Decide whether or not this user has a home guild set.
 
         :return: True if this user has a home guild, False otherwise
         :rtype: bool
         """
-        return self.homeGuild is not None
+        return self.homeGuildID != -1
 
 
     def canTransferGuild(self, now=datetime.utcnow()) -> bool:
-        """Decide whether this user is allowed to transfer their homeGuild.
+        """Decide whether this user is allowed to transfer their homeGuildID.
         This is decided based on the time passed since their last guild transfer.
 
         :param datetime.datetime now: The current time, if known. This optional parameter is included for increasing efficiency in the case where the current time has already been calculated.
         :return: True if this user has no home guild, or their guild transfer cooldown has completed, false otherwise
         :rtype: bool
         """
-        return not self.hasHomeGuild() or datetime.utcnow() > self.guildTransferCooldownEnd
+        return not self.hashomeGuild() or datetime.utcnow() > self.guildTransferCooldownEnd
 
     
     def transferGuild(self, newGuild : Guild):
-        """Transfer the user's homeGuild to the given guild.
+        """Transfer the user's homeGuildID to the given guild.
         The user must not be on guild transfer cooldown.
 
-        :param discord.Guild newGuild: The new discord.Guild to set this user's homeGuild to. This user must be a member of newGuild.
+        :param discord.Guild newGuild: The new discord.Guild to set this user's homeGuildID to. This user must be a member of newGuild.
         :raise ValueError: When this user is still in guild transfer cooldown
         :raise NameError: When this user is not a member of newGuild
         """
@@ -626,8 +626,8 @@ class bbUser:
         if newGuild.get_member(self.id) is None:
             raise NameError("This user is not a member of the given guild '" + newGuild.name + "#" + str(newGuild.id) + "'")
         
-        self.homeGuild = newGuild
-        self.guildTransferCooldownEnd = now + bbUtil.timeDeltaFromDict(bbConfig.homeGuildTransferCooldown)
+        self.homeGuildID = newGuild.id
+        self.guildTransferCooldownEnd = now + bbUtil.timeDeltaFromDict(bbConfig.homeGuildIDTransferCooldown)
 
 
     def __str__(self) -> str:
@@ -636,7 +636,7 @@ class bbUser:
         :return: A string summar of the user, containing the user ID and home guild ID.
         :rtype: str
         """
-        return "<bbUser #" + str(self.id) + ((" @" + str(self.homeGuild.id)) if self.homeGuild is not None else "") + ">"
+        return "<bbUser #" + str(self.id) + ((" @" + str(self.homeGuildID)) if self.hashomeGuildID() else "") + ">"
 
 
 def fromDict(id : int, userDict : dict) -> bbUser:
@@ -682,5 +682,5 @@ def fromDict(id : int, userDict : dict) -> bbUser:
                     bountyWinsToday=userDict["bountyWinsToday"] if "bountyWinsToday" in userDict else 0,
                     dailyBountyWinsReset=datetime.utcfromtimestamp(userDict["dailyBountyWinsReset"]) if "dailyBountyWinsReset" in userDict else datetime.utcnow(),
                     pollOwned=userDict["pollOwned"] if "pollOwned" in userDict else False,
-                    homeGuild=userDict["homeGuild"] if "homeGuild" in userDict else -1,
+                    homeGuildID=userDict["homeGuildID"] if "homeGuildID" in userDict else -1,
                     guildTransferCooldownEnd=datetime.utcfromtimestamp(userDict["guildTransferCooldownEnd"]) if "guildTransferCooldownEnd" in userDict else datetime.utcnow())
