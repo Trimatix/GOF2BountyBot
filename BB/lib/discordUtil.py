4f53096423c5d9e53715733f6e3e333bd2a8ccf8
Trimatix <1jasperlaw@gmail.com>
2020-11-13 14:45:37 +0000

    move bountybot.getMemberByRefOverDB, typeAlertedUserMentionOrName and IDAlertedUserMentionOrName to lib.discordUtil

--- BB/lib/discordUtil.py
+++ BB/lib/discordUtil.py
@@ -9,6 +9,7 @@ from ..logging import bbLogger
 from . import stringTyping
 from .. import bbGlobals
 from discord import Embed, Colour
+from ..userAlerts import UserAlerts
 
 
 def findBBUserDCGuild(user : bbUser.bbUser) -> Union[Guild, None]:
@@ -177,3 +178,88 @@ def makeEmbed(titleTxt="", desc="", col=Colour.blue(), footerTxt="", img="", thu
     if icon != "":
         embed.set_author(name=authorName, icon_url=icon)
     return embed
+
+
+def getMemberByRefOverDB(uRef : str, dcGuild=None) -> User:
+    """Attempt to get a user object from a given string user reference.
+    a user reference can be one of:
+    - A user mention <@123456> or <@!123456>
+    - A user ID 123456
+    - A user name Carl
+    - A user name and discriminator Carl#0324
+
+    If uRef is not a user mention or ID, dcGuild must be provided, to be searched for the given name.
+    When validating a name uRef, the process is much more efficient when also given the user's discriminator.
+
+    :param str uRef: A string or integer indentifying the user object to look up
+    :param discord.Guild dcGuild: The guild in which to search for a user identified by uRef. Required if uRef is not a mention or ID. (Default None)
+    :return: Either discord.member of a member belonging to dcGuild and matching uRef, or None if uRef is invalid or no matching user could be found
+    :rtype: discord.Member or None
+    """
+    if dcGuild is not None:
+        userAttempt = getMemberFromRef(uRef, dcGuild)
+    else:
+        userAttempt = None
+    if userAttempt is None and stringTyping.isInt(uRef):
+        if bbGlobals.usersDB.userIDExists(int(uRef)):
+            userGuild = findBBUserDCGuild(bbGlobals.usersDB.getUser(int(uRef)))
+            if userGuild is not None:
+                return userGuild.get_member(int(uRef))
+    return userAttempt
+
+
+def typeAlertedUserMentionOrName(alertType : UserAlerts.UABase, dcUser=None, bbUser=None, bbGuild=None, dcGuild=None) -> str:
+    """If the given user has subscribed to the given alert type, return the user's mention. Otherwise, return their display name and discriminator.
+    At least one of dcUser or bbUser must be provided.
+    bbGuild and dcGuild are both optional. If neither are provided then the joined guilds will be searched for the given user.
+    This means that giving at least one of bbGuild or dcGuild will drastically improve efficiency.
+    TODO: rename bbUser and bbGuild so it doesnt match the class name
+
+    :param UserAlerts.UABase alertType: The type of alert to check the state of
+    :param discord.User dcUser: The user to check the alert state of. One of dcUser or bbUser is required. (Default None)
+    :param bbUser bbUser: The user to check the alert state of. One of dcUser or bbUser is required. (Default None)
+    :param bbGuild bbGuild: The guild in which to check the alert state. Optional, but improves efficiency. (Default None)
+    :param dcGuild dcGuild: The guild in which to check the alert state. Optional, but improves efficiency. (Default None)
+    :return: If the given user is alerted for the given type in the selected guild, the user's mention. The user's display name and discriminator otherwise.
+    :rtype: str
+    :raise ValueError: When given neither dcUser nor bbUser
+    :raise KeyError: When given neither bbGuild nor dcGuild, and the user could not be located in any of the bot's joined guilds.
+    """
+    if dcUser is None and bbUser is None:
+        raise ValueError("At least one of dcUser or bbUser must be given.")
+
+    if bbGuild is None and dcGuild is None:
+        dcGuild = findBBUserDCGuild(dcUser)
+        if dcGuild is None:
+            raise KeyError("user does not share an guilds with the bot")
+    if bbGuild is None:
+        bbGuild = bbGlobals.guildsDB.getGuild(dcGuild.id)
+    elif dcGuild is None:
+        dcGuild = bbGlobals.client.get_guild(bbGuild.id)
+    if bbUser is None:
+        bbUser = bbGlobals.usersDB.getOrAddID(dcUser.id)
+
+    guildMember = dcGuild.get_member(dcUser.id)
+    if guildMember is None:
+        return dcUser.name + "#" + str(dcUser.discriminator)
+    if bbUser.isAlertedForType(alertType, dcGuild, bbGuild, dcUser):
+        return guildMember.mention
+    return guildMember.display_name + "#" + str(guildMember.discriminator)
+
+
+def IDAlertedUserMentionOrName(alertID : str, dcUser=None, bbUser=None, bbGuild=None, dcGuild=None) -> str:
+    """If the given user has subscribed to the alert type of the given ID, return the user's mention. Otherwise, return their display name and discriminator.
+    At least one of dcUser or bbUser must be provided.
+    bbGuild and dcGuild are both optional. If neither are provided then the joined guilds will be searched for the given user.
+    This means that giving at least one of bbGuild or dcGuild will drastically improve efficiency.
+    TODO: rename bbUser and bbGuild so it doesnt match the class name
+
+    :param UserAlerts.UABase alertType: The ID, according to UserAlerts.userAlertsIDsTypes, of type of alert to check the state of
+    :param discord.User dcUser: The user to check the alert state of. One of dcUser or bbUser is required. (Default None)
+    :param bbUser bbUser: The user to check the alert state of. One of dcUser or bbUser is required. (Default None)
+    :param bbGuild bbGuild: The guild in which to check the alert state. Optional, but improves efficiency. (Default None)
+    :param dcGuild dcGuild: The guild in which to check the alert state. Optional, but improves efficiency. (Default None)
+    :return: If the given user is alerted for the given type in the selected guild, the user's mention. The user's display name otherwise.
+    :rtype: str
+    """
+    return typeAlertedUserMentionOrName(UserAlerts.userAlertsIDsTypes[alertID], dcUser=dcUser, bbUser=bbUser, bbGuild=bbGuild, dcGuild=dcGuild)
