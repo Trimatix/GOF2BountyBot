55ff270d40619018193552587ce9d110afaf3a15
Trimatix <1jasperlaw@gmail.com>
2020-11-16 23:12:46 +0000

    update bbToolItemFactory.fromDict to support bbCrates

--- BB/bbObjects/items/tools/bbToolItemFactory.py
+++ BB/bbObjects/items/tools/bbToolItemFactory.py
@@ -1,6 +1,6 @@
-from . import bbToolItem, bbShipSkinTool
-
-toolTypeConstructors = {"bbShipSkinTool": bbShipSkinTool.fromDict}
+from . import bbToolItem, bbShipSkinTool, bbCrate
+from .. import bbShip, bbWeapon, bbModuleFactory, bbTurret
+from .... import lib
 
 def fromDict(toolDict : dict) -> bbToolItem.bbToolItem:
     """Construct a bbToolItem from its dictionary-serialized representation.
@@ -11,6 +11,33 @@ def fromDict(toolDict : dict) -> bbToolItem.bbToolItem:
     :rtype: bbToolItem.bbToolItem
     :raise NameError: When toolDict does not contain a 'type' attribute.
     """
+
+    itemConstructors = {"bbShip": bbShip.fromDict,
+                    "bbWeapon": bbWeapon.fromDict,
+                    "bbModule": bbModuleFactory.fromDict,
+                    "bbTurret": bbTurret.fromDict,
+                    "bbToolItem": fromDict}
+
+    def crateFromDict(crateDict):
+        if "itemPool" not in crateDict:
+            print("CRATEDICT",crateDict)
+            raise RuntimeError()
+        itemPool = []
+        for itemDict in crateDict["itemPool"]:
+            itemPool.append(itemConstructors[itemDict["itemType"]](itemDict))
+        
+        return bbCrate.bbCrate(itemPool, name=crateDict["name"] if "name" in crateDict else "",
+            value=crateDict["value"] if "value" in crateDict else 0,
+            wiki=crateDict["wiki"] if "wiki" in crateDict else "",
+            manufacturer=crateDict["manufacturer"] if "manufacturer" in crateDict else "",
+            icon=crateDict["icon"] if "icon" in crateDict else "",
+            emoji=lib.emojis.dumbEmojiFromDict(crateDict["emoji"]) if "emoji" in crateDict else lib.emojis.dumbEmoji.EMPTY,
+            techLevel=crateDict["techLevel"] if "techLevel" in crateDict else -1,
+            builtIn=crateDict["builtIn"] if "builtIn" in crateDict else False)
+
+    toolTypeConstructors = {"bbShipSkinTool": bbShipSkinTool.fromDict,
+                        "bbCrate": crateFromDict}
+    
     if "type" not in toolDict:
         raise NameError("Required dictionary attribute missing: 'type'")
     return toolTypeConstructors[toolDict["type"]](toolDict)
