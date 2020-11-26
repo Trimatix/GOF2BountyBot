ff15e1676b320346941442e900c7afe67fccf005
Trimatix <1jasperlaw@gmail.com>
2020-11-26 11:27:39 +0000

    add kwargs to saveDB funcs. add placeholder prettyPrint kwarg, to be filled out in a skins branch merge later on

--- BB/lib/jsonHandler.py
+++ BB/lib/jsonHandler.py
@@ -14,7 +14,7 @@ def readJSON(dbFile : str) -> dict:
     return json.loads(txt)
 
 
-def writeJSON(dbFile : str, db : dict):
+def writeJSON(dbFile : str, db : dict, prettyPrint=False):
     """Write the given json-serializable dictionary to the given file path. All objects in the dictionary must be JSON-serializable.
     TODO: Check this makes the file if it doesnt exist
 
@@ -27,17 +27,17 @@ def writeJSON(dbFile : str, db : dict):
     f.close()
 
 
-def saveDB(dbPath : str, db):
+def saveDB(dbPath : str, db, **kwargs):
     """Call the given database object's toDict method, and save the resulting dictionary to the specified JSON file.
     TODO: child database classes to a single ABC, and type check to that ABC here before saving
 
     :param str dbPath: path to the JSON file to save to. Theoretically, this can be absolute or relative.
     :param db: the database object to save
     """
-    writeJSON(dbPath, db.toDict())
+    writeJSON(dbPath, db.toDict(**kwargs))
 
 
-async def saveDBAsync(dbPath : str, db):
+async def saveDBAsync(dbPath : str, db, **kwargs):
     """This function should be used in place of saveDB for database objects whose toDict method is asynchronous.
     This function is currently unused.
 
@@ -47,4 +47,4 @@ async def saveDBAsync(dbPath : str, db):
     :param str dbPath: path to the JSON file to save to. Theoretically, this can be absolute or relative.
     :param db: the database object to save
     """
-    writeJSON(dbPath, await db.toDict())
+    writeJSON(dbPath, await db.toDict(**kwargs))
