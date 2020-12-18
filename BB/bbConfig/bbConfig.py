0b5661b2eb701e6c4f62ba9d335148c64cad84e6
Trimatix <1jasperlaw@gmail.com>
2020-12-18 23:27:43 +0000

    add usr_kaamo to loaded commands modules

--- BB/bbConfig/bbConfig.py
+++ BB/bbConfig/bbConfig.py
@@ -22,7 +22,7 @@ def truncToRes(num : float) -> float:
 
 
 # List of module names from BB.commands to import
-includedCommandModules = (  "usr_misc", "usr_homeguilds", "usr_gof2-info", "usr_bounties", "usr_loadout", "usr_economy",
+includedCommandModules = (  "usr_misc", "usr_homeguilds", "usr_gof2-info", "usr_bounties", "usr_loadout", "usr_economy", "usr_kaamo",
                             "admn_channels", "admn_misc",
                             "dev_misc", "dev_channels", "dev_bounties", "dev_items", "dev_skins")
 
