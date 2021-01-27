a5c6959a83ae726676f1e058dd7eef938bae5de0
Trimatix <1jasperlaw@gmail.com>
2021-01-27 01:29:23 +0000

    move showme res to 400x and samples to 8

--- BB/bbConfig/bbConfig.py
+++ BB/bbConfig/bbConfig.py
@@ -282,8 +282,8 @@ skinRenderIconSamples = 8
 skinRenderEmojiResolution = [400, 400]
 skinRenderEmojiSamples = 8
 # Resolution of skin renders from cmd_showme_ship calls
-skinRenderShowmeResolution = [352, 240]
-skinRenderShowmeSamples = 128
+skinRenderShowmeResolution = [400, 400] #[352, 240]
+skinRenderShowmeSamples = 8
 # Resolution of skin renders from admin_cmd_showmeHD calls
 skinRenderShowmeHDResolution = [1920, 1080]
 skinRenderShowmeHDSamples = 4
